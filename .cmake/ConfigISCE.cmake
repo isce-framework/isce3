0cb9ec937415b1e7dfa8a32d10fe686d381ffeb4
Piyush Agram <agram@jpl.nasa.gov>
2017-12-18 11:21:42 -0800

    Added cmake skeleton

---
+++ .cmake/ConfigISCE.cmake
@@ -0,0 +1,63 @@
+###Custom function to prevent in-source builds
+function(AssureOutOfSourceBuilds)
+    get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
+    get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
+
+    if("${srcdir}" STREQUAL "${bindir}")
+        message("################################")
+        message(" ISCE should not be configured and built in the soutce directory")
+        message(" You must run cmake in a build directory. ")
+        message(" When directory structure is finalized .. can add full example here")
+        message(FATAL_ERROR "Quitting. In-source builds not allowed....")
+    endif()
+endfunction()
+
+function(CheckISCEPython)
+    FIND_PACKAGE(PythonInterp 3.5)
+    FIND_PACKAGE(PythonInterp 3.5)
+    include(useCython)
+endfunction()
+
+
+function(InitInstallDirLayout)
+    ###install/bin
+    if (NOT ISCE_BINDIR)
+        set (ISCE_BINDIR bin)
+    endif(NOT ISCE_BINDIR)
+
+    ###install/packages
+    if (NOT ISCE_PACKAGESDIR)
+        set (ISCE_PACKAGESDIR packages)
+    endif(NOT ISCE_PACKAGESDIR)
+
+    ###install/lib
+    if (NOT ISCE_LIBDIR)
+        set(ISCE_LIBDIR lib)
+    endif(NOT ISCE_LIBDIR)
+
+    ###install/include
+    if (NOT ISCE_INCLUDEDIR)
+        set(ISCE_INCLUDEDIR include)
+    endif(NOT ISCE_INCLUDEDIR)
+
+    ###install/defaults
+    if (NOT ISCE_DEFAULTSDIR)
+        set(ISCE_DEFAULTSDIR defaults)
+    endif(NOT ISCE_DEFAULTSDIR)
+
+    ###install/var
+    if (NOT ISCE_VARDIR)
+        set(ISCE_VARDIR var)
+    endif(NOT ISCE_VARDIR)
+
+    ###install/etc
+    if (NOT ISCE_ETCDIR)
+        set(ISCE_ETCDIR etc)
+    endif(NOT ISCE_ETCDIR)
+
+    ###install/templates
+    if (NOT ISCE_TEMPLATESDIR)
+        set(ISCE_TEMPLATESDIR templates)
+    endif(NOT ISCE_TEMPLATESDIR)
+endfunction()
+
