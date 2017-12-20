c4ef1b52e80abf9374a420ba28f21d17484f73da
Eric Gurrola <eric.m.gurrola@jpl.nasa.gov>
2017-12-20 10:50:16 -0800

    Adding contrib directory with googletest subdirectory.

---
+++ contrib/googletest/include/gtest/Make.mm
@@ -0,0 +1,55 @@
+# -*- Makefile -*-
+#
+# eric m. gurrola
+# Jet Propulsion Lab/Caltech
+# (c) 2017 all rights reserved
+#
+
+# project global settings
+PROJECT = gtest
+
+# the package name
+PACKAGE =
+
+# my subdirectories
+RECURSE_DIRS = internal
+
+# the top level headers
+EXPORT_HEADERS = \
+    gtest.h \
+    gtest-death-test.h \
+    gtest-message.h \
+    gtest-param-test.h \
+    gtest-printers.h \
+    gtest-spi.h \
+    gtest-test-part.h \
+    gtest-typed-test.h \
+    gtest_pred_impl.h \
+    gtest_prod.h \
+
+all: export
+	BLD_ACTION="all" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+live:
+	BLD_ACTION="live" $(MM) recurse
+
+export:: export-headers
+        BLD_ACTION="export $(MM) recurse
+
+# shortcuts for building specific subdirectories
+.PHONY: $(RECURSE_DIRS)
+
+$(RECURSE_DIRS):
+	(cd $@; $(MM))
+
+
+# end of file
