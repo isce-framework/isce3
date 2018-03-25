7267ba074c33f6ba6d370830589eb5fe3fb80bb6
Lavalle <Marco.Lavalle@jpl.nasa.gov>
2018-03-25 16:16:21 -0700

    Added multi-band VRT Raster creation; significant refactoring in Raster class

--- lib/isce/core/Raster.cpp
+++ lib/isce/core/Raster.cpp
@@ -1,7 +1,7 @@
 //-*- C++ -*-
 //-*- coding: utf-8 -*-
 //
-// Author: ml
+// Author: Marco Lavalle
 // Original code: Joshua Cohen
 // Copyright 2018
 //
@@ -14,32 +14,23 @@
 #include "gdal_priv.h"
 #include "Raster.h"
     
-// Unordered_map to map typeids to GDALDataTypes
-const std::unordered_map<std::type_index, GDALDataType> isce::core::Raster::_gdts =
-  {{typeid(uint8_t),               GDT_Byte},
-   {typeid(uint16_t),              GDT_UInt16},
-   {typeid(int16_t),               GDT_Int16},
-   {typeid(uint32_t),              GDT_UInt32},
-   {typeid(int32_t),               GDT_Int32},
-   {typeid(float),                 GDT_Float32},
-   {typeid(double),                GDT_Float64},
-   {typeid(std::complex<int16_t>), GDT_CInt16},
-   {typeid(std::complex<int32_t>), GDT_CInt32},
-   {typeid(std::complex<float>),   GDT_CFloat32},
-   {typeid(std::complex<double>),  GDT_CFloat64}};
 
 
 // Construct a Raster object referring to existing file
-isce::core::Raster::Raster(const std::string &fname,          // filename
-			   GDALAccess access) {   // GA_ReadOnly or GA_Update
+isce::core::Raster::Raster(const std::string &fname,   // filename
+			   GDALAccess access) {        // GA_ReadOnly or GA_Update
 
   GDALAllRegister();  // GDAL checks internally if drivers are already loaded
   dataset( static_cast<GDALDataset*>(GDALOpenShared(fname.c_str(), access)) );
 }
 
+
+
 // Construct a Raster object referring to existing file
 isce::core::Raster::Raster(const std::string &fname) :
   isce::core::Raster(fname, GA_ReadOnly) {}
+
+
 
 // Construct a Raster object referring to new file
 isce::core::Raster::Raster(const std::string &fname,          // filename 
@@ -54,25 +45,37 @@ isce::core::Raster::Raster(const std::string &fname,          // filename
   dataset( outputDriver->Create (fname.c_str(), width, length, numBands, dtype, NULL) );
 }
 
+
+
 // Construct a Raster object referring to new file assuming default GDAL driver.
 isce::core::Raster::Raster(const std::string &fname, size_t width, size_t length, size_t numBands, GDALDataType dtype) :
   isce::core::Raster(fname, width, length, numBands, dtype, isce::core::defaultGDALDriver) {}
+
+
 
 // Construct a Raster object referring to new file assuming default GDAL driver and dataype.
 isce::core::Raster::Raster(const std::string &fname, size_t width, size_t length, size_t numBands) :
   isce::core::Raster(fname, width, length, numBands, isce::core::defaultGDALDataType) {}
 
+
+
 // Construct a Raster object referring to new file assuming default GDAL driver and band.
 isce::core::Raster::Raster(const std::string &fname, size_t width, size_t length, GDALDataType dtype) :
   isce::core::Raster(fname, width, length, 1, dtype, isce::core::defaultGDALDriver) {}
+
+
 
 // Construct a Raster object referring to new file assuming default GDAL driver, dataype and band.
 isce::core::Raster::Raster(const std::string &fname, size_t width, size_t length) :
   isce::core::Raster(fname, width, length, 1) {}
 
+
+
 // Construct a Raster object referring to new file assuming default GDAL driver, dataype and band.
 isce::core::Raster::Raster(const std::string &fname, const Raster &rast) :
   isce::core::Raster(fname, rast.width(), rast.length(), rast.numBands(), rast.dtype()) {}
+
+
 
 // Copy constructor. It increments GDAL's reference counter after weak-copying the pointer
 isce::core::Raster::Raster(const Raster &rast) {
@@ -81,36 +84,21 @@ isce::core::Raster::Raster(const Raster &rast) {
 }
 
 
+
 // Construct a Raster object referring to a VRT dataset with multiple bands from a vector
 // of Raster objects. Input rasters with multiple bands are unfolded within the output raster
-// ToDo: Add member function isce::core::Raster::mergeRaster and call that function from here
-isce::core::Raster::Raster(const std::string& fname, const std::vector<Raster>& rastVec) {
-  
-  for (auto r : rastVec)  // all rasters must have the same size
-    if ( !r.match(rastVec.front()) ) 
-      throw std::length_error("In isce::core::Raster::Raster() - Rasters have different sizes.");
-  
+isce::core::Raster::Raster(const std::string& fname, const std::vector<Raster>& rastVec) {  
   GDALAllRegister();
   GDALDriver * outputDriver = GetGDALDriverManager()->GetDriverByName("VRT");
   dataset( outputDriver->Create (fname.c_str(),
 				 rastVec.front().width(),
 				 rastVec.front().length(),
-				 0,    // bands will be added later
+				 0,    // bands are added below
 				 rastVec.front().dtype(),
 				 NULL) );  
 
-  for ( auto r : rastVec)                       // for each input Raster object in rastVec
-    for ( size_t b=1; b<=r.numBands(); ++b ) {  // for each band in input Raster object
-      dataset()->AddBand( r.dtype(b), NULL );   // add the band to the output VRT dataset
-      GDALRasterBand *tmp_band = dataset()->GetRasterBand( numBands() ); 
-      VRTAddSimpleSource(tmp_band,
-			 r.dataset()->GetRasterBand(b),
-			 0, 0,
-			 r.width(), r.length(), 
-			 0, 0,
-			 r.width(), r.length(),
-			 NULL, VRT_NODATA_UNSET);
-    }
+  for ( auto r : rastVec)     // for each input Raster object in rastVec
+    addRasterToVRT( r );
 }
 
 
@@ -121,12 +109,5 @@ isce::core::Raster::~Raster() {
   GDALClose( _dataset );
 }
 
-
-// Load a dataset on existing Raster object after releasing the old dataset
-void isce::core::Raster::open(const std::string &fname,
-			      GDALAccess access=GA_ReadOnly) {
-  GDALClose( _dataset );
-  dataset( static_cast<GDALDataset*>(GDALOpenShared(fname.c_str(), access)) );
-}
 
 // end of file
--- lib/isce/core/Raster.h
+++ lib/isce/core/Raster.h
@@ -1,7 +1,7 @@
 //-*- C++ -*-
 //-*- coding: utf-8 -*-
 //
-// Author: ml
+// Author: Marco Lavalle
 // Original code: Joshua Cohen
 // Copyright 2018
 //
@@ -20,6 +20,8 @@
 #include "gdal_vrt.h"
 #include "Constants.h"
 
+//#include <pyre/journal.h>
+
 namespace isce {
   namespace core {
     
@@ -27,9 +29,9 @@ namespace isce {
       
     public:
       
-      // Constructors and destructors
+      // Constructors and destructor
       // Open existing file: (filename, [GA_ReadOnly or GDAL_Update])
-      // Note from gdal.h: typedef enum { GA_ReadOnly=0, GA_Update=1 } GDALAccess; 
+      // From gdal.h: typedef enum { GA_ReadOnly=0, GA_Update=1 } GDALAccess; 
       // Create new file: (filename, width, length, numBands[=1], datatype[=default], GDALdriver[=default])
       //                  (filename, STL container to infer width and datatype, length)
       Raster(const std::string&);
@@ -40,29 +42,30 @@ namespace isce {
       Raster(const std::string&, size_t, size_t, GDALDataType);
       Raster(const std::string&, size_t, size_t);
       Raster(const std::string&, const Raster&);
-      template<typename T> Raster(const std::string&, const std::vector<T>&,      size_t);
-      template<typename T> Raster(const std::string&, const std::valarray<T>&,    size_t);
+      template<typename T> Raster(const std::string&, const std::vector<T>&,   size_t);
+      template<typename T> Raster(const std::string&, const std::valarray<T>&, size_t);
       Raster(const std::string&, const std::vector<Raster>&);
       Raster(const Raster&);
       ~Raster();
 
       // Operators, getters and setters
-      inline Raster& operator=(const Raster&);
-      inline GDALDataset* dataset()  const { return _dataset; }
-      inline void         dataset(GDALDataset* ds) { _dataset=ds; }
+      inline Raster&      operator=(const Raster&);
       inline size_t       length()   const { return _dataset->GetRasterYSize(); }
       inline size_t       width()    const { return _dataset->GetRasterXSize(); }
       inline size_t       numBands() const { return _dataset->GetRasterCount(); }
       inline GDALAccess   access()   const { return _dataset->GetAccess(); }
+      inline GDALDataset* dataset()  const { return _dataset; }
+      inline void         dataset(GDALDataset* ds) { _dataset=ds; }
       inline GDALDataType dtype(const size_t band=1) const { return _dataset->GetRasterBand(band)->GetRasterDataType(); }
       inline bool         match(const Raster & rast) const { return width()==rast.width() && length()==rast.length(); }  
-
-      void open(const std::string&, GDALAccess);
+      inline void         open(const std::string&, GDALAccess);
+      inline void         addRasterToVRT(const isce::core::Raster&);
+      inline void         addBandToVRT(GDALRasterBand *);
       //void close() { GDALClose( _dataset ); }  // todo: fix segfault conflict with destructor
       
         
-      // Pixel read/write, optional band index	  
-      // (buffer, x-index, y-index, [band-index=1])
+      // Pixel read/write with optional band index	  
+      // (buffer, x-index, y-index, band-index[=1])
       template<typename T> void getSetValue(T&, size_t, size_t, size_t, GDALRWFlag);
 
       template<typename T> void getValue(T&, size_t, size_t, size_t);
@@ -72,47 +75,47 @@ namespace isce {
       template<typename T> void setValue(T&, size_t, size_t);
 
      
-      // Line read/write, generic container w/ width or STL container, optional band index
-      // (buffer, line-index, nelem, [band-index=1])
+      // Line read/write with raw pointer and width or STL container, optional band index
+      // (buffer, line-index, nelem, band-index[=1])
       template<typename T> void getSetLine(T*, size_t, size_t, size_t, GDALRWFlag);
       
       template<typename T> void getLine(T*, size_t, size_t, size_t);
       template<typename T> void getLine(T*, size_t, size_t);
       template<typename T> void getLine(std::vector<T>&, size_t, size_t);
       template<typename T> void getLine(std::vector<T>&, size_t);
-      template<typename T> void getLine(std::valarray<T>&,size_t,size_t);
-      template<typename T> void getLine(std::valarray<T>&,size_t);
+      template<typename T> void getLine(std::valarray<T>&, size_t, size_t);
+      template<typename T> void getLine(std::valarray<T>&, size_t);
       
-      template<typename T> void setLine(T*,size_t,size_t,size_t);
-      template<typename T> void setLine(T*,size_t,size_t);
-      template<typename T> void setLine(std::vector<T>&,size_t,size_t);
-      template<typename T> void setLine(std::vector<T>&,size_t);
-      template<typename T> void setLine(std::valarray<T>&,size_t,size_t);
-      template<typename T> void setLine(std::valarray<T>&,size_t);
+      template<typename T> void setLine(T*, size_t, size_t, size_t);
+      template<typename T> void setLine(T*, size_t, size_t);
+      template<typename T> void setLine(std::vector<T>&, size_t, size_t);
+      template<typename T> void setLine(std::vector<T>&, size_t);
+      template<typename T> void setLine(std::valarray<T>&, size_t, size_t);
+      template<typename T> void setLine(std::valarray<T>&, size_t);
 
       
       // 2D block read/write, generic container w/ width or STL container, optional band index
       // (buffer, x-index, y-index, nXelem, nYelem, [band-index=1])
-      template<typename T> void getSetBlock(T*,size_t,size_t,size_t,size_t,size_t, GDALRWFlag);
+      template<typename T> void getSetBlock(T*, size_t, size_t, size_t, size_t, size_t, GDALRWFlag);
 
-      template<typename T> void getBlock(T*,size_t,size_t,size_t,size_t,size_t);
-      template<typename T> void getBlock(T*,size_t,size_t,size_t,size_t);
-      template<typename T> void getBlock(std::vector<T>&,size_t,size_t,size_t,size_t,size_t);
-      template<typename T> void getBlock(std::vector<T>&,size_t,size_t,size_t,size_t);
-      template<typename T> void getBlock(std::valarray<T>&,size_t,size_t,size_t,size_t,size_t);
-      template<typename T> void getBlock(std::valarray<T>&,size_t,size_t,size_t,size_t);
+      template<typename T> void getBlock(T*, size_t, size_t, size_t, size_t, size_t);
+      template<typename T> void getBlock(T*, size_t, size_t, size_t, size_t);
+      template<typename T> void getBlock(std::vector<T>&, size_t, size_t, size_t, size_t, size_t);
+      template<typename T> void getBlock(std::vector<T>&, size_t, size_t, size_t, size_t);
+      template<typename T> void getBlock(std::valarray<T>&, size_t, size_t, size_t, size_t, size_t);
+      template<typename T> void getBlock(std::valarray<T>&, size_t, size_t, size_t, size_t);
 
-      template<typename T> void setBlock(T*,size_t,size_t,size_t,size_t,size_t);
-      template<typename T> void setBlock(T*,size_t,size_t,size_t,size_t);
-      template<typename T> void setBlock(std::vector<T>&,size_t,size_t,size_t,size_t,size_t);
-      template<typename T> void setBlock(std::vector<T>&,size_t,size_t,size_t,size_t);
-      template<typename T> void setBlock(std::valarray<T>&,size_t,size_t,size_t,size_t,size_t);
-      template<typename T> void setBlock(std::valarray<T>&,size_t,size_t,size_t,size_t);
+      template<typename T> void setBlock(T*, size_t, size_t, size_t, size_t, size_t);
+      template<typename T> void setBlock(T*, size_t, size_t, size_t, size_t);
+      template<typename T> void setBlock(std::vector<T>&, size_t, size_t, size_t, size_t, size_t);
+      template<typename T> void setBlock(std::vector<T>&, size_t, size_t, size_t, size_t);
+      template<typename T> void setBlock(std::valarray<T>&, size_t, size_t, size_t, size_t, size_t);
+      template<typename T> void setBlock(std::valarray<T>&, size_t, size_t, size_t, size_t);
       
     private:
       
       GDALDataset * _dataset; 
-      static const std::unordered_map<std::type_index, GDALDataType> _gdts;
+
     };
   }
 }
