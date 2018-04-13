ab55319f10d90bd1367b0c1d3c89518c908bff25
Bryan Riel <briel@jpl.nasa.gov>
2018-04-13 16:51:21 -0700

    Make.mm now deletes output files in resampslc test.

--- tests/lib/isce/core/resampslc/Make.mm
+++ tests/lib/isce/core/resampslc/Make.mm
@@ -21,7 +21,7 @@ test: $(TESTS)
             } done
 
 # build
-PROJ_CLEAN += $(TESTS)
+PROJ_CLEAN += $(TESTS) warped.slc warped.slc.xml
 PROJ_CXX_INCLUDES += $(EXPORT_ROOT)/include/$(PROJECT)-$(PROJECT_MAJOR).$(PROJECT_MINOR)
 PROJ_LIBRARIES = -lisce.$(PROJECT_MAJOR).$(PROJECT_MINOR) -lgtest -fopenmp
 LIBRARIES = $(PROJ_LIBRARIES) $(EXTERNAL_LIBS)
