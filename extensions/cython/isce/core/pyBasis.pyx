22844743093453c7e8c8afd1912a0c97c0fd1b80
piyush.agram@jpl.nasa.gov <agram@jpl.nasa.gov>
2018-07-03 17:44:57 -0700

    Fix memory views

--- extensions/cython/isce/core/pyBasis.pyx
+++ extensions/cython/isce/core/pyBasis.pyx
@@ -4,10 +4,14 @@
 # Copyright 2017-2018
 #
 
-import numpy as np
 from Basis cimport Basis
+import numpy as np
+cimport numpy as np
 
 cdef class pyBasis:
+    '''
+    Python wrapper for isce::core::Basis
+    '''
 
     cdef Basis c_basis
 
@@ -16,38 +20,86 @@ cdef class pyBasis:
 
     @property
     def x0(self):
-        return [self.c_basis.x0()[i] for i in range(3)]
+        '''
+        Return the first basis vector.
+
+        Returns:
+            numpy.array(3)
+        '''
+        #Make a memory view and copy
+        cdef cartesian_t x0 = self.c_basis.x0()
+        res = np.asarray((<double[:3]>(&x0[0])).copy())
+        return res
+
     @x0.setter
     def x0(self, x):
+        '''
+        Set the first basis vector.
+
+        Args:
+            x (list or numpy.array(3)): list of floats
+        '''
         self._checklist(x)
         cdef cartesian_t cx
-        cdef int i
-        for i in range(3):
-            cx[i] = x[i]
+        cdef int ii
+        for ii in range(3):
+            cx[ii] = x[ii]
         self.c_basis.x0(cx)
 
     @property
     def x1(self):
-        return [self.c_basis.x1()[i] for i in range(3)]
+        '''
+        Return the second basis vector.
+
+        Returns:
+            numpy.array(3)
+        '''
+        #Make a memory view and copy
+        cdef cartesian_t x1 = self.c_basis.x1()
+        res = np.asarray((<double[:3]>(&x1[0])).copy())
+        return res
+
     @x1.setter
     def x1(self, x):
+        '''
+        Set the second basis vector.
+
+        Args:
+            x (list or numpy.array(3)): list of floats
+        '''
         self._checklist(x)
         cdef cartesian_t cx
-        cdef int i
-        for i in range(3):
-            cx[i] = x[i]
+        cdef int ii
+        for ii in range(3):
+            cx[ii] = x[ii]
         self.c_basis.x1(cx)
 
     @property
     def x2(self):
-        return [self.c_basis.x2()[i] for i in range(3)]
+        '''
+        Return the third basis vector.
+
+        Returns:
+            numpy.array(3)
+        '''
+        #Make a memory view and copy
+        cdef cartesian_t x2 = self.c_basis.x2()
+        res = np.asarray((<double[:3]>(&x2[0])).copy())
+        return res
+    
     @x2.setter
     def x2(self, x):
+        '''
+        Set the third basis vector.
+
+        Args:
+            x (list or numpy.array(3)): list of floats
+        '''
         self._checklist(x)
         cdef cartesian_t cx
-        cdef int i
-        for i in range(3):
-            cx[i] = x[i]
+        cdef int ii
+        for ii in range(3):
+            cx[ii] = x[ii]
         self.c_basis.x2(cx)
 
     @staticmethod
@@ -56,10 +108,10 @@ cdef class pyBasis:
         Check the norm and length of the input list.
         """
         assert len(x) == 3, 'Input basis vector does not have three-elements'
-        cdef int i
+        cdef int ii
         cdef double xsum = 0.0
-        for i in range(3):
-            xsum += x[i]**2
+        for ii in range(3):
+            xsum += x[ii]**2
         assert abs(xsum - 1.0) < 1.0e-8, 'Input basis vector not a unit vector'
         return
 
