f023ea2d97f32d785bc63fac26859052c0a0ff44
Bryan Riel <briel@jpl.nasa.gov>
2018-08-07 15:41:43 -0700

    Files for supporting new image namespace.

---
+++ lib/isce/image/Constants.h
@@ -0,0 +1,24 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// Author: Bryan V. Riel
+// Copyright 2017-2018
+//
+
+#ifndef ISCE_IMAGE_CONSTANTS_H
+#define ISCE_IMAGE_CONSTANTS_H
+
+namespace isce {
+    namespace image {
+
+        const int SINC_HALF = 4;
+        const int SINC_LEN = 8;
+        const int SINC_ONE = 9;
+        const int SINC_SUB = 8192;
+
+    }
+}
+
+#endif
+
+// end of file
