904f6b97ef88eaf656cbed66e1bd7933ec54ac21
Liang Yu <liang.yu@jpl.nasa.gov>
2018-08-15 17:57:47 -0700

    compiling constructors

--- extensions/cuda/gpuPoly2d.h
+++ extensions/cuda/gpuPoly2d.h
@@ -28,31 +28,29 @@ namespace isce { namespace core { namespace cuda {
         double rangeNorm;
         double azimuthNorm;
         double *coeffs;
-    }
     
-    CUDA_HOSTDEV gpuPoly2d gpuPoly2d = 0;
-    CUDA_HOSTDEV gpuPoly2d(int ro, int ao, double rm, double am, double rn, double an) : rangeOrder(ro), 
-                                                                                         azimuthOrder(ao), 
-                                                                                         rangeMean(rm), 
-                                                                                         azimuthMean(am),
-                                                                                         rangeNorm(rn), 
-                                                                                         azimuthNorm(an),
-                                                                                         coeffs(0.)
-                                                                                         {}
-    CUDA_HOSTDEV gpuPoly2d() : gpuPoly2d(-1,-1,0.,0.,1.,1.,0.) {}
+        CUDA_HOSTDEV gpuPoly2d(int ro, int ao, double rm, double am, double rn, double an) : rangeOrder(ro), 
+                                                                                             azimuthOrder(ao), 
+                                                                                             rangeMean(rm), 
+                                                                                             azimuthMean(am),
+                                                                                             rangeNorm(rn), 
+                                                                                             azimuthNorm(an)
+                                                                                             //coeffs(0.)
+                                                                                             {}
+        CUDA_HOSTDEV gpuPoly2d() : gpuPoly2d(-1,-1,0.,0.,1.,1.) {}
 
-    CUDA_DEV gpuPoly2d(const gpuPoly &p)
-    gpuPoly2d(const gpuPoly2d &p) : rangeOrder(p.rangeOrder), azimuthOrder(p.azimuthOrder), 
-                                    rangeMean(p.rangeMean), azimuthMean(p.azimuthMean), 
-                                    rangeNorm(p.rangeNorm), azimuthNorm(p.azimuthNorm), 
-                                    coeffs(p.coeffs) {}
-    CUDA_HOST gpuPoly2d(const Poly2d&);
-    CUDA_HOSTDEV gpuPoly2d& operator=(const gpuPoly2d&) = delete;
-    ~gpuPoly2d();
+        CUDA_DEV gpuPoly2d(const gpuPoly2d &p) : rangeOrder(p.rangeOrder), azimuthOrder(p.azimuthOrder), 
+                                                rangeMean(p.rangeMean), azimuthMean(p.azimuthMean), 
+                                                rangeNorm(p.rangeNorm), azimuthNorm(p.azimuthNorm), 
+                                                coeffs(p.coeffs) {}
+        CUDA_HOST gpuPoly2d(const Poly2d&);
+        CUDA_HOSTDEV gpuPoly2d& operator=(const gpuPoly2d&) = delete;
+        ~gpuPoly2d();
+    };
 
-    CUDA_HOST setCoeff();
+    //CUDA_HOST setCoeff();
 
-    CUDA_HOST eval_h(double, double);
+    //CUDA_HOST eval_h(double, double);
 }}}
 
 #endif
