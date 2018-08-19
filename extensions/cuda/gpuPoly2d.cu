f89c127dcd3f2d1b20ee4cb42fcc3b8f53018da1
Liang Yu <liang.yu@jpl.nasa.gov>
2018-08-19 12:26:11 -0700

    compiling but untested gpuPoly

--- extensions/cuda/gpuPoly2d.cu
+++ extensions/cuda/gpuPoly2d.cu
@@ -20,7 +20,8 @@ __host__ gpuPoly2d::gpuPoly2d(const Poly2d &poly) :
     rangeMean(poly.rangeMean), 
     azimuthMean(poly.azimuthMean),
     rangeNorm(poly.rangeNorm), 
-    azimuthNorm(poly.azimuthNorm)
+    azimuthNorm(poly.azimuthNorm),
+    owner(true)
 {
     cudaSetDevice(0);
     
@@ -28,3 +29,80 @@ __host__ gpuPoly2d::gpuPoly2d(const Poly2d &poly) :
     cudaMalloc((double**)&coeffs, n_coeffs*sizeof(double));
     cudaMemcpy(coeffs, &(poly.coeffs[0]), n_coeffs*sizeof(double), cudaMemcpyHostToDevice);
 }
+
+
+gpuPoly2d::~gpuPoly2d() {
+    if (owner) {
+        cudaFree(coeffs);
+    }
+}
+
+__device__ double gpuPoly2d::eval(double azi, double rng) {
+
+    double xval = (rng - rangeMean) / rangeNorm;
+    double yval = (azi - azimuthMean) / azimuthNorm;
+
+    double scalex;
+    double scaley = 1.;
+    double val = 0.;
+    for (int i=0; i<=azimuthOrder; i++,scaley*=yval) {
+        scalex = 1.;
+        for (int j=0; j<=rangeOrder; j++,scalex*=xval) {
+            val += scalex * scaley * coeffs[IDX1D(i,j,rangeOrder+1)];
+        }
+    }
+    return val;
+}
+
+__global__ void eval_d(gpuPoly2d p, double azi, double rng, double *val)
+{
+    *val = p.eval(azi, rng);
+}
+
+__host__ double gpuPoly2d::eval_h(double azi, double rng)
+{
+    cudaSetDevice(0);
+    double *val_d;
+    double val_h;
+    // use unified memory?
+    cudaMalloc((double**)&val_d, sizeof(double));
+    dim3 grid(1), block(1);
+    eval_d<<<grid,block>>>(*this, azi, rng, val_d);
+    cudaMemcpy(&val_h, val_d, sizeof(double), cudaMemcpyDeviceToHost);
+    cudaFree(val_d);
+    return val_h;
+}
+
+/*
+void isce::core::Poly2d::
+setCoeff(int row, int col, double val) {
+    if ((row < 0) || (row > azimuthOrder)) {
+        std::string errstr = "Poly2d::setCoeff - Trying to set coefficient for row " + 
+                             std::to_string(row+1) + " out of " + 
+                             std::to_string(azimuthOrder+1);
+        throw std::out_of_range(errstr);
+    }
+    if ((col < 0) || (col > rangeOrder)) {
+        std::string errstr = "Poly2d::setCoeff - Trying to set coefficient for col " +
+                             std::to_string(col+1) + " out of " + std::to_string(rangeOrder+1);
+        throw std::out_of_range(errstr);
+    }
+    coeffs[IDX1D(row,col,rangeOrder+1)] = val;
+}
+
+double isce::core::Poly2d::
+getCoeff(int row, int col) const {
+    if ((row < 0) || (row > azimuthOrder)) {
+        std::string errstr = "Poly2d::getCoeff - Trying to get coefficient for row " +
+                             std::to_string(row+1) + " out of " + 
+                             std::to_string(azimuthOrder+1);
+        throw std::out_of_range(errstr);
+    }
+    if ((col < 0) || (col > rangeOrder)) {
+        std::string errstr = "Poly2d::getCoeff - Trying to get coefficient for col " + 
+                             std::to_string(col+1) + " out of " + std::to_string(rangeOrder+1);
+        throw std::out_of_range(errstr);
+    }
+    return coeffs[IDX1D(row,col,rangeOrder+1)];
+}
+*/
--- extensions/cuda/gpuPoly2d.h
+++ extensions/cuda/gpuPoly2d.h
@@ -28,29 +28,45 @@ namespace isce { namespace core { namespace cuda {
         double rangeNorm;
         double azimuthNorm;
         double *coeffs;
+        bool owner;
     
         CUDA_HOSTDEV gpuPoly2d(int ro, int ao, double rm, double am, double rn, double an) : rangeOrder(ro), 
                                                                                              azimuthOrder(ao), 
                                                                                              rangeMean(rm), 
                                                                                              azimuthMean(am),
                                                                                              rangeNorm(rn), 
-                                                                                             azimuthNorm(an)
-                                                                                             //coeffs(0.)
+                                                                                             azimuthNorm(an), 
+                                                                                             owner(false)
                                                                                              {}
         CUDA_HOSTDEV gpuPoly2d() : gpuPoly2d(-1,-1,0.,0.,1.,1.) {}
-
         CUDA_DEV gpuPoly2d(const gpuPoly2d &p) : rangeOrder(p.rangeOrder), azimuthOrder(p.azimuthOrder), 
                                                 rangeMean(p.rangeMean), azimuthMean(p.azimuthMean), 
                                                 rangeNorm(p.rangeNorm), azimuthNorm(p.azimuthNorm), 
                                                 coeffs(p.coeffs) {}
         CUDA_HOST gpuPoly2d(const Poly2d&);
-        CUDA_HOSTDEV gpuPoly2d& operator=(const gpuPoly2d&) = delete;
         ~gpuPoly2d();
+
+        CUDA_HOSTDEV inline gpuPoly2d& operator=(const gpuPoly2d&);
+
+        CUDA_DEV double eval(double,double);
+        CUDA_HOST double eval_h(double, double); 
     };
+
+
+    CUDA_HOSTDEV inline gpuPoly2d& gpuPoly2d::operator=(const gpuPoly2d &rhs)
+    {
+        rangeOrder = rhs.rangeOrder;
+        azimuthOrder = rhs.azimuthOrder;
+        rangeMean = rhs.rangeMean;
+        azimuthMean = rhs.azimuthMean;
+        rangeNorm = rhs.rangeNorm;
+        azimuthNorm = rhs.azimuthNorm;
+        coeffs = rhs.coeffs;
+        return *this;
+    }
 
     //CUDA_HOST setCoeff();
 
-    //CUDA_HOST eval_h(double, double);
 }}}
 
 #endif
