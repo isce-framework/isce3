27578b572f001f5c27843c2b5a3cf240cd32fd13
Bryan V Riel <briel@SNA-DGX-N01.jpl.nasa.gov>
2018-09-07 09:07:34 -0700

    Moving cuda linalg routines to .cu file

--- extensions/cuda/core/gpuLinAlg.cu
+++ extensions/cuda/core/gpuLinAlg.cu
@@ -3,5 +3,44 @@
 // Copyright 2017
 //
 
+#include <cmath>
 #include "gpuLinAlg.h"
 
+__device__ void
+isce::cuda::core::gpuLinAlg::
+cross(double *u, double *v, double *w) {
+    w[0] = (u[1] * v[2]) - (u[2] * v[1]);
+    w[1] = (u[2] * v[0]) - (u[0] * v[2]);
+    w[2] = (u[0] * v[1]) - (u[1] * v[0]);
+}
+
+__device__ double
+isce::cuda::core::gpuLinAlg::
+dot(double *u, double *v) {
+    return ((u[0]*v[0]) + (u[1]*v[1]) + (u[2]*v[2]));
+}
+
+__device__ void
+isce::cuda::core::gpuLinAlg::
+linComb(double a, double *u, double b, double *v, double *w) {
+    w[0] = (a * u[0]) + (b * v[0]);
+    w[1] = (a * u[1]) + (b * v[1]);
+    w[2] = (a * u[2]) + (b * v[2]);
+}
+
+__device__ void
+isce::cuda::core::gpuLinAlg::
+unitVec(double *v, double *vhat) {
+    double mag = norm(v);
+    vhat[0] = v[0] / mag;
+    vhat[1] = v[1] / mag;
+    vhat[2] = v[2] / mag;
+}
+
+__device__ double
+isce::cuda::core::gpuLinAlg::
+norm(double *v) {
+    return std::sqrt(std::pow(v[0], 2) + std::pow(v[1], 2) + std::pow(v[2], 2));
+}
+
+// end of file
--- extensions/cuda/core/gpuLinAlg.h
+++ extensions/cuda/core/gpuLinAlg.h
@@ -3,48 +3,31 @@
 // Copyright 2017
 //
 
-#ifndef __ISCE_CUDA_CORE_GPULINALG_H__
-#define __ISCE_CUDA_CORE_GPULINALG_H__
+#ifndef ISCE_CUDA_CORE_GPULINALG_H
+#define ISCE_CUDA_CORE_GPULINALG_H
 
-#include <cmath>
+#ifdef __CUDACC__
+#define CUDA_HOSTDEV __host__ __device__
+#define CUDA_DEV __device__
+#define CUDA_HOST __host__
+#else
+#define CUDA_HOSTDEV
+#define CUDA_DEV
+#define CUDA_HOST
+#endif
 
 namespace isce { namespace cuda { namespace core {
     struct gpuLinAlg {
-        __host__ __device__ gpuLinAlg() = delete;
+        CUDA_HOSTDEV gpuLinAlg() = delete;
 
-        __device__ static inline void cross(double*,double*,double*);
-        __device__ static inline double dot(double*,double*);
-        __device__ static inline void linComb(double,double*,double,double*,double*);
-        __device__ static inline void unitVec(double*,double*);
-        __device__ static inline double norm(double*);
+        CUDA_DEV static void cross(double*,double*,double*);
+        CUDA_DEV static double dot(double*,double*);
+        CUDA_DEV static void linComb(double,double*,double,double*,double*);
+        CUDA_DEV static void unitVec(double*,double*);
+        CUDA_DEV static double norm(double*);
     };
-
-    __device__ inline void gpuLinAlg::cross(double *u, double *v, double *w) {
-        w[0] = (u[1] * v[2]) - (u[2] * v[1]);
-        w[1] = (u[2] * v[0]) - (u[0] * v[2]);
-        w[2] = (u[0] * v[1]) - (u[1] * v[0]);
-    }
-
-    __device__ inline double gpuLinAlg::dot(double *u, double *v) {
-        return ((u[0]*v[0]) + (u[1]*v[1]) + (u[2]*v[2]));
-    }
-
-    __device__ inline void gpuLinAlg::linComb(double a, double *u, double b, double *v, double *w) {
-        w[0] = (a * u[0]) + (b * v[0]);
-        w[1] = (a * u[1]) + (b * v[1]);
-        w[2] = (a * u[2]) + (b * v[2]);
-    }
-
-    __device__ inline void gpuLinAlg::unitVec(double *v, double *vhat) {
-        double mag = norm(v);
-        vhat[0] = v[0] / mag;
-        vhat[1] = v[1] / mag;
-        vhat[2] = v[2] / mag;
-    }
-
-    __device__ inline double gpuLinAlg::norm(double *v) {
-        return std::sqrt(std::pow(v[0], 2) + std::pow(v[1], 2) + std::pow(v[2], 2));
-    }
 }}}
 
 #endif
+
+// end of file
