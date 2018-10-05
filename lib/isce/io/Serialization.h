9a4ab6ae3aedb76cef33a4b8eac2162f699ab983
Bryan Riel <briel@jpl.nasa.gov>
2018-10-05 15:50:16 -0700

    Updated HDF5 serialization interface to use H5 groups throughout.

--- lib/isce/io/Serialization.h
+++ lib/isce/io/Serialization.h
@@ -21,41 +21,41 @@ namespace isce {
     //! The isce::io namespace
     namespace io {
 
-        /** Load scalar dataset from HDF5.
+        /** Load scalar dataset from HDF5 file.
          *
-         * @param[in] file          HDF5 file object.
-         * @param[in] datasetPath   H5 path of dataset.
+         * @param[in] h5obj         HDF5 file or group object.
+         * @param[in] datasetPath   H5 path of dataset relative to h5obj.
          * @param[in] v             Scalar return value. */
-        template <typename T>
-        inline void loadFromH5(isce::io::IH5File & file, const std::string & datasetPath, T & v) {
+        template <typename H5obj, typename T>
+        inline void loadFromH5(H5obj & h5obj, const std::string & datasetPath, T & v) {
             // Open dataset
-            isce::io::IDataSet dataset = file.openDataSet(datasetPath);
+            isce::io::IDataSet dataset = h5obj.openDataSet(datasetPath);
             // Read the scalar dataset
             dataset.read(v);
         }
 
-        /** Load vector dataset from HDF5.
+        /** Load vector dataset from HDF5 file.
          *
-         * @param[in] file          HDF5 file object.
-         * @param[in] datasetPath   H5 path of dataset.
+         * @param[in] file          HDF5 file or group object.
+         * @param[in] datasetPath   H5 path of dataset relative to h5obj.
          * @param[in] v             Vector to store dataset. */
-        template <typename T>
-        inline void loadFromH5(isce::io::IH5File & file, const std::string & datasetPath,
-                        std::vector<T> & v) {
+        template <typename H5obj, typename T>
+        inline void loadFromH5(H5obj & h5obj, const std::string & datasetPath,
+                               std::vector<T> & v) {
             // Open dataset
-            isce::io::IDataSet dataset = file.openDataSet(datasetPath);
+            isce::io::IDataSet dataset = h5obj.openDataSet(datasetPath);
             // Read the vector dataset
             dataset.read(v);
         }
 
-        /** Get dimensions of complex imagery from HDF5.
+        /** Get dimensions of complex imagery from HDF5 file.
          *
-         * @param[in] file          HDF5 file object.
-         * @param[in] datasetPath   H5 path of image dataset. */
-        inline std::vector<int> getImageDims(isce::io::IH5File & file,
-                                      const std::string & datasetPath) {
+         * @param[in] file          HDF5 file or group object.
+         * @param[in] datasetPath   H5 path of image dataset relative to h5obj. */
+        template <typename H5obj>
+        inline std::vector<int> getImageDims(H5obj & h5obj, const std::string & datasetPath) {
             // Open dataset
-            isce::io::IDataSet dataset = file.openDataSet(datasetPath);
+            isce::io::IDataSet dataset = h5obj.openDataSet(datasetPath);
             // Get dimensions
             return dataset.getDimensions();
         }
