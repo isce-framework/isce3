9f1788ac872fc59d72239902fe019fb51c6412f8
Bryan Riel <briel@jpl.nasa.gov>
2018-10-09 10:30:46 -0700

    Moved processing options to constructor.

--- extensions/cython/isce/geometry/pyGeo2rdr.pyx
+++ extensions/cython/isce/geometry/pyGeo2rdr.pyx
@@ -15,6 +15,10 @@ cdef class pyGeo2rdr:
 
     Args:
         product (pyProduct):                 Configured Product.
+        threshold (Optional[float]):         Threshold for iteration stop for slant range.
+        numIterations (Optional[int]):       Max number of normal iterations.
+        orbitMethod (Optional[str]):         Orbit interpolation method
+                                                ('hermite', 'sch', 'legendre')
 
     Return:
         None
@@ -30,19 +34,25 @@ cdef class pyGeo2rdr:
         'legendre': orbitInterpMethod.LEGENDRE_METHOD
     }
 
-    def __cinit__(self, pyProduct product):
+    def __cinit__(self, pyProduct product, threshold=1.0e-5, numIterations=50,
+                  orbitMethod='hermite'):
         """
         Constructor takes in a product in order to retrieve relevant radar parameters.
         """
+        # Create C++ geo2rdr pointer
         self.c_geo2rdr = new Geo2rdr(deref(product.c_product))
         self.__owner = True
+
+        # Set processing options
+        self.c_geo2rdr.threshold(threshold)
+        self.c_geo2rdr.numiter(numIterations)
+        self.c_geo2rdr.orbitMethod(self.orbitInterpMethods[orbitMethod])
 
     def __dealloc__(self):
         if self.__owner:
             del self.c_geo2rdr
 
-    def geo2rdr(self, pyRaster topoRaster, outputDir, double azshift=0.0, double rgshift=0.0,
-        threshold=1.0e-5, numIterations=50, orbitMethod='hermite'):
+    def geo2rdr(self, pyRaster topoRaster, outputDir, double azshift=0.0, double rgshift=0.0):
         """
         Run geo2rdr.
         
@@ -50,25 +60,15 @@ cdef class pyGeo2rdr:
             topoRaster (pyRaster):              Raster for topo products.
             outputDir (str):                    String for output directory.
             azshift (Optional[double]):         Constant azimuth offset.
-            rgshift (Optional[double]):         Constant range offset.
-            threshold (Optional[float]):        Threshold for iteration stop for slant range.
-            numIterations (Optional[int]):      Max number of normal iterations.
-            orbitMethod (Optional[str]):        Orbit interpolation method
-                                                    ('hermite', 'sch', 'legendre')
+            rgshift (Optional[double]):         Constant range offset. 
 
         Return:
             None
         """
-        # Set processing options
-        self.c_geo2rdr.threshold(threshold)
-        self.c_geo2rdr.numiter(numIterations)
-        self.c_geo2rdr.orbitMethod(self.orbitInterpMethods[orbitMethod])
-
         # Convert output directory to C++ string
         cdef string outdir = pyStringToBytes(outputDir)
 
         # Run geo2rdr
         self.c_geo2rdr.geo2rdr(deref(topoRaster.c_raster), outdir, azshift, rgshift)
-
 
 # end of file
