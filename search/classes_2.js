72d502462673500060d62caf5aeb95f9cfb22e85
gmanipon <gmanipon@jpl.nasa.gov>
2018-11-14 03:16:31 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/isce-develop-nightly-build/45/)

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['cea',['CEA',['../structisce_1_1core_1_1CEA.html',1,'isce::core']]],
-  ['compleximagery',['ComplexImagery',['../classisce_1_1product_1_1ComplexImagery.html',1,'isce::product']]],
-  ['crossmul',['Crossmul',['../classisce_1_1signal_1_1Crossmul.html',1,'isce::signal']]]
+  ['compleximagery',['ComplexImagery',['../classisce_1_1product_1_1ComplexImagery.html',1,'isce::product']]]
 ];
