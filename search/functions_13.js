613184f4edc7b673e3165ef0cb06b4a7dad4555b
gmanipon <gmanipon@jpl.nasa.gov>
2018-12-18 00:21:57 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/PR-isce-develop/43/)

--- search/functions_13.js
+++ search/functions_13.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['unitvec',['unitVec',['../structisce_1_1core_1_1LinAlg.html#acc8cbc22ebddd97ae72a1dedbd319572',1,'isce::core::LinAlg']]],
   ['updateutctimes',['updateUTCTimes',['../structisce_1_1core_1_1Orbit.html#ae052ae1828c53f6bb0b8feeb6837922c',1,'isce::core::Orbit']]],
-  ['upsample',['upsample',['../classisce_1_1signal_1_1Signal.html#a259eaa5b9c89404fcacb2e9caa29c246',1,'isce::signal::Signal']]]
+  ['upsample',['upsample',['../classisce_1_1signal_1_1Signal.html#a259eaa5b9c89404fcacb2e9caa29c246',1,'isce::signal::Signal::upsample(std::valarray&lt; std::complex&lt; T &gt;&gt; &amp;signal, std::valarray&lt; std::complex&lt; T &gt;&gt; &amp;signalOversampled, int rows, int nfft, int oversampleFactor)'],['../classisce_1_1signal_1_1Signal.html#a1c3c4b058c26b8f97aa589f0054a45a0',1,'isce::signal::Signal::upsample(std::valarray&lt; std::complex&lt; T &gt;&gt; &amp;signal, std::valarray&lt; std::complex&lt; T &gt;&gt; &amp;signalOversampled, int rows, int nfft, int oversampleFactor, std::valarray&lt; std::complex&lt; T &gt;&gt; shiftImpact)']]]
 ];
