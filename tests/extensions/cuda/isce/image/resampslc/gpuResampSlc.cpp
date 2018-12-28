12c1016c18b5d6a972c8ef750beaf921b13d5c0c
Liang Yu <liang.yu@jpl.nasa.gov>
2018-12-28 11:54:04 -0800

    fixed relative paths to test data

--- tests/extensions/cuda/isce/image/resampslc/gpuResampSlc.cpp
+++ tests/extensions/cuda/isce/image/resampslc/gpuResampSlc.cpp
@@ -34,37 +34,43 @@
 TEST(ResampSlcTest, Resamp) {
 
     // Open the HDF5 product
-    std::string h5file("../../data/envisat.h5");
+    //std::string h5file("../../data/envisat.h5");
+    // tests/lib/isce/data
+    // tests/lib/isce/image/resamp
+    // tests/extensions/cuda/isce/image/resamplpslc
+    std::string h5file("/home/lyu/tools/isce/gpu_build/tests/lib/isce/data/envisat.h5");
     isce::io::IH5File file(h5file);
 
     // Create product
     isce::product::Product product(file);
 
     // Instantiate a ResampSLC object
-    isce::cuda::image::ResampSlc resamp(product);
+    isce::cuda::image::ResampSlc gpu_resamp(product);
+    isce::image::ResampSlc cpu_resamp(product);
 
     // Use same product as a reference
-    resamp.referenceProduct(product);
+    gpu_resamp.referenceProduct(product);
     
     // Check values
-    ASSERT_NEAR(resamp.imageMode().startingRange(), 826988.6900674499, 1.0e-10);
-    ASSERT_NEAR(resamp.doppler().coeffs[0], 301.35306906319204, 1.0e-8);
-
-    // Perform resampling with default lines per tile
-    resamp.resamp("warped.slc", "hh",
-                  "../../data/offsets/range.off", "../../data/offsets/azimuth.off");
+    ASSERT_NEAR(gpu_resamp.imageMode().startingRange(), 826988.6900674499, 1.0e-10);
+    ASSERT_NEAR(gpu_resamp.doppler().coeffs[0], 301.35306906319204, 1.0e-8);
+    // Perform gpu_resampling with default lines per tile
+    cpu_resamp.resamp("c_warped.slc", "hh",
+                  "../../../../../lib/isce/data/offsets/range.off", "../../../../../lib/isce/data/offsets/azimuth.off");
+    gpu_resamp.resamp("g_warped.slc", "hh",
+                  "../../../../../lib/isce/data/offsets/range.off", "../../../../../lib/isce/data/offsets/azimuth.off");
     
     // Set lines per tile to be a weird multiple of the number of output lines
-    resamp.linesPerTile(249);
-    // Re-run resamp
-    resamp.resamp("warped.slc", "hh",
-                  "../../data/offsets/range.off", "../../data/offsets/azimuth.off");
+    gpu_resamp.linesPerTile(249);
+    // Re-run gpu_resamp
+    gpu_resamp.resamp("warped.slc", "hh",
+                  "../../../../../lib/isce/data/offsets/range.off", "../../../../../lib/isce/data/offsets/azimuth.off");
 }
 
 // Compute sum of difference between reference image and warped image
 TEST(ResampSlcTest, Validate) {
     // Open SLC rasters 
-    isce::io::Raster refSlc("../../data/warped_envisat.slc.vrt");
+    isce::io::Raster refSlc("../../../../../lib/isce/data/warped_envisat.slc.vrt");
     isce::io::Raster testSlc("warped.slc");
     // Compute total complex error
     std::complex<float> sum(0.0, 0.0);
