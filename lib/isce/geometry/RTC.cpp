1619470bc191603570c0371750801c46b4d04ae4
Ryan Burns <rtburns@jpl.nasa.gov>
2019-01-16 15:35:42 -0800

    Fix upscaling factor

--- lib/isce/geometry/RTC.cpp
+++ lib/isce/geometry/RTC.cpp
@@ -79,7 +79,7 @@ double computeUpsamplingFactor(const isce::geometry::DEMInterpolator& dem_interp
     const double radarArea = mode.rangePixelSpacing() * mode.rangePixelSpacing();
 
     // Upsampling factor is the ratio
-    return demArea / radarArea;
+    return std::sqrt(demArea / radarArea);
 }
 
 void facetRTC(isce::product::Product& product, isce::io::Raster& dem, isce::io::Raster& out_raster) {
