68bce18a1c1cef4b5f3caed0c087109dedf17466
gmanipon <gmanipon@jpl.nasa.gov>
2019-01-17 20:02:55 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/isce-develop/88/)

--- search/all_2.js
+++ search/all_2.js
@@ -13,5 +13,6 @@ var searchData=
   ['azimuthlooks',['azimuthLooks',['../classisce_1_1signal_1_1Crossmul.html#a8259371554fcc2c0ab1011d6bee1ccdb',1,'isce::signal::Crossmul']]],
   ['azimuthmean',['azimuthMean',['../structisce_1_1core_1_1Poly2d.html#a8ef844f41400b1e867ec41f08d779ada',1,'isce::core::Poly2d']]],
   ['azimuthnorm',['azimuthNorm',['../structisce_1_1core_1_1Poly2d.html#a37da4f4990f225094c99505eca80d5e0',1,'isce::core::Poly2d']]],
-  ['azimuthorder',['azimuthOrder',['../structisce_1_1core_1_1Poly2d.html#a03ff9aee7b05163f06e26e572eebbe5d',1,'isce::core::Poly2d']]]
+  ['azimuthorder',['azimuthOrder',['../structisce_1_1core_1_1Poly2d.html#a03ff9aee7b05163f06e26e572eebbe5d',1,'isce::core::Poly2d']]],
+  ['azimuthpixelspacing',['azimuthPixelSpacing',['../classisce_1_1product_1_1ImageMode.html#acc0d1a5e3ec1d3feb7ead0d9906a4db6',1,'isce::product::ImageMode::azimuthPixelSpacing() const'],['../classisce_1_1product_1_1ImageMode.html#a2e00547485f20c536de70bc91793261c',1,'isce::product::ImageMode::azimuthPixelSpacing(double value)']]]
 ];
