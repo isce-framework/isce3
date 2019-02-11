6a473ba9d77285e532ef651b684c31a5c0831dfb
Ryan Burns <rtburns@jpl.nasa.gov>
2019-02-11 15:12:37 -0800

    Working preliminary GPU RTC code

---
+++ extensions/cuda/geometry/gpuRTC.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "isce/product/Product.h"
+#include "isce/io/Raster.h"
+
+namespace isce { namespace cuda { namespace geometry {
+    void facetRTC(isce::product::Product& product,
+                  isce::io::Raster& dem,
+                  isce::io::Raster& out_raster);
+};};};
