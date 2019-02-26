9372b1ee237379c59d02914018f9b973ca8a184b
gmanipon <gmanipon@jpl.nasa.gov>
2019-02-26 02:03:04 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/PR-isce-develop/122/)

--- search/classes_73.js
+++ search/classes_73.js
@@ -4,6 +4,5 @@ var searchData=
   ['signal',['Signal',['../classisce_1_1signal_1_1Signal.html',1,'isce::signal']]],
   ['sinc2dinterpolator',['Sinc2dInterpolator',['../classisce_1_1core_1_1Sinc2dInterpolator.html',1,'isce::core']]],
   ['spline2dinterpolator',['Spline2dInterpolator',['../classisce_1_1core_1_1Spline2dInterpolator.html',1,'isce::core']]],
-  ['statevector',['StateVector',['../classisce_1_1core_1_1StateVector.html',1,'isce::core']]],
-  ['swath',['Swath',['../classisce_1_1product_1_1Swath.html',1,'isce::product']]]
+  ['statevector',['StateVector',['../classisce_1_1core_1_1StateVector.html',1,'isce::core']]]
 ];
