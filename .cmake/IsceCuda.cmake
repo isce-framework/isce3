2baa3e1c78784cd8221ed624d2f9950a98177a84
Ryan Burns <rtburns@jpl.nasa.gov>
2019-02-27 09:23:01 -0800

    Don't manually set CUDA host compiler

--- .cmake/IsceCuda.cmake
+++ .cmake/IsceCuda.cmake
@@ -1,5 +1,2 @@
 # Set the name for the ISCE C++/CUDA library
 set(LISCECUDA iscecuda.${ISCE_VERSION_MAJOR}.${ISCE_VERSION_MINOR})
-
-# Use correct host compiler with NVCC
-set(CUDA_HOST_COMPILER "${CMAKE_CXX_COMPILER}")
