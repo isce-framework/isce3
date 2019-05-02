5dc6129425797a41ce15af3615a1ea0e70ca7519
Ryan Burns <rtburns@jpl.nasa.gov>
2019-05-02 16:16:56 -0700

    Add test

--- tests/extensions/cuda/isce/except/checkCudaErrors.cpp
+++ tests/extensions/cuda/isce/except/checkCudaErrors.cpp
@@ -1,11 +1,23 @@
 #include <cuda_runtime.h>
 #include <gtest/gtest.h>
 #include <isce/cuda/except/Error.h>
+#include <cufft.h>
+#include <thrust/device_vector.h>
 
 // Just ensure that checkCudaErrors throws an ISCE CudaError
 TEST(CheckCudaErrors, BasicThrow) {
     EXPECT_THROW(checkCudaErrors(cudaSetDevice(-1)),
                  isce::cuda::except::CudaError<cudaError_t>);
+}
+
+// Check that a non-zero cufftResult throws an error
+// Make sure an error is thrown when executing an FFT before setting up a plan
+TEST(CheckCudaErrors, CufftResult) {
+    cufftHandle plan;
+    thrust::device_vector<cufftComplex> signal;
+    cufftComplex* data = signal.data().get();
+    EXPECT_THROW(checkCudaErrors(cufftExecC2C(plan, data, data, CUFFT_FORWARD)),
+                 isce::cuda::except::CudaError<cufftResult>);
 }
 
 int main(int argc, char* argv[]) {
