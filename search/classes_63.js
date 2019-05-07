0db877c572eeb908e8299b53cd24af4307deff21
gmanipon <gmanipon@jpl.nasa.gov>
2019-05-07 05:23:40 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/isce-develop-nightly-build/230/)

--- search/classes_63.js
+++ search/classes_63.js
@@ -6,6 +6,5 @@ var searchData=
   ['changedetector',['ChangeDetector',['../classChangeDetector.html',1,'']]],
   ['covariance',['Covariance',['../classisce_1_1signal_1_1Covariance.html',1,'isce::signal']]],
   ['crossmul',['Crossmul',['../classisce_1_1signal_1_1Crossmul.html',1,'isce::signal']]],
-  ['cube',['Cube',['../classisce_1_1core_1_1Cube.html',1,'isce::core']]],
   ['cudaerror',['CudaError',['../structisce_1_1cuda_1_1except_1_1CudaError.html',1,'isce::cuda::except']]]
 ];
