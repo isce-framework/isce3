6f6de77931a00f250ded505c10aa9e02366c5e38
gmanipon <gmanipon@jpl.nasa.gov>
2019-05-13 21:31:14 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/PR-isce-develop/240/)

--- search/functions_62.js
+++ search/functions_62.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['b',['b',['../classisce_1_1core_1_1Ellipsoid.html#a3c51d951c46cdab9ce56e301ed25ef63',1,'isce::core::Ellipsoid']]],
+  ['b',['b',['../classisce_1_1core_1_1Ellipsoid.html#abf4e3634f6464e27a5f08b3ac32038c4',1,'isce::core::Ellipsoid']]],
   ['bandwidth',['bandwidth',['../classisce_1_1core_1_1KnabKernel.html#a2a5d839cd5b092f54ec6f225a7e55bb5',1,'isce::core::KnabKernel']]],
   ['bartlettkernel',['BartlettKernel',['../classisce_1_1core_1_1BartlettKernel.html#a6b0e4f30407956426cb0d315a8c53b07',1,'isce::core::BartlettKernel']]],
   ['baseline',['Baseline',['../classisce_1_1core_1_1Baseline.html#a64b6dae4f4ee5a826ab833ca79d1f8a9',1,'isce::core::Baseline::Baseline()'],['../classisce_1_1core_1_1Baseline.html#a1dfb03e3819615d525442e8e6757ebaa',1,'isce::core::Baseline::Baseline(const Baseline &amp;b)']]],
-  ['basis',['Basis',['../classisce_1_1core_1_1Basis.html#ab16823535fbcbcee1228e6bd654638fa',1,'isce::core::Basis::Basis()'],['../classisce_1_1core_1_1Basis.html#a996035044956f0b968cf4157875f225f',1,'isce::core::Basis::Basis(const Vec3 &amp;x0, const Vec3 &amp;x1, const Vec3 &amp;x2)']]],
+  ['basis',['Basis',['../classisce_1_1core_1_1Basis.html#a61d26157ac702fcfadf1046f2a912cdc',1,'isce::core::Basis::Basis()'],['../classisce_1_1core_1_1Basis.html#a7c343bfeb6b93ab225074ff53ee9df59',1,'isce::core::Basis::Basis(cartesian_t &amp;x0, cartesian_t &amp;x1, cartesian_t &amp;x2)']]],
   ['beta',['beta',['../classisce_1_1signal_1_1Crossmul.html#af251748003cb48812a1e884faee4715e',1,'isce::signal::Crossmul::beta()'],['../classisce_1_1cuda_1_1signal_1_1gpuCrossmul.html#a0d0a2b7ac9390857b064db197aaf6cc2',1,'isce::cuda::signal::gpuCrossmul::beta()']]],
   ['bicubicinterpolator',['BicubicInterpolator',['../classisce_1_1core_1_1BicubicInterpolator.html#a14ba55881438ccb62445786af38fabab',1,'isce::core::BicubicInterpolator']]],
   ['bilinearinterpolator',['BilinearInterpolator',['../classisce_1_1core_1_1BilinearInterpolator.html#a1ca5c850e4eae19408b470ac55e14a3a',1,'isce::core::BilinearInterpolator']]],
