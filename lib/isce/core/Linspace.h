17a5a98ee90367046e6ed5f12cc35f94a364d25f
Geoffrey M Gunter <geoffrey.m.gunter@jpl.nasa.gov>
2019-05-21 15:33:18 -0700

    Add Linspace
     - Add new class isce::core::Linspace
     - Add unit tests for isce::core::Linspace

---
+++ lib/isce/core/Linspace.h
@@ -0,0 +1,127 @@
+#pragma once
+#ifndef ISCE_CORE_LINSPACE_H
+#define ISCE_CORE_LINSPACE_H
+
+#include <stddef.h>
+
+#include "Common.h"
+
+namespace isce { namespace core {
+
+/** A uniformly-spaced sequence of values over some interval. */
+template<typename T>
+class Linspace {
+public:
+    /**
+     * Construct a Linspace over the closed interval [first, last].
+     *
+     * The behavior is undefined if size <= 1.
+     *
+     * @param[in] first first sample in interval
+     * @param[in] last last sample in interval
+     * @param[in] size number of samples
+     */
+    CUDA_HOSTDEV
+    static
+    Linspace<T> from_interval(T first, T last, size_t size);
+
+    Linspace() = default;
+
+    /**
+     * Constructor
+     *
+     * @param[in] first first sample
+     * @param[in] spacing sample spacing
+     * @param[in] size number of samples
+     */
+    CUDA_HOSTDEV
+    constexpr
+    Linspace(T first, T spacing, size_t size);
+
+    /** Copy constructor */
+    template<typename U>
+    CUDA_HOSTDEV
+    Linspace(const Linspace<U> &);
+
+    /** Assign values. */
+    template<typename U>
+    CUDA_HOSTDEV
+    Linspace<T> & operator=(const Linspace<U> &);
+
+    /**
+     * Return sample at the specified position.
+     *
+     * The behavior is undefined if pos is out of range (i.e. if it is >= size()).
+     */
+    CUDA_HOSTDEV
+    constexpr
+    T operator[](size_t pos) const;
+
+    /** First sample */
+    CUDA_HOSTDEV
+    constexpr
+    T first() const;
+
+    /** Last sample */
+    CUDA_HOSTDEV
+    constexpr
+    T last() const;
+
+    /** Sample spacing */
+    CUDA_HOSTDEV
+    constexpr
+    T spacing() const;
+
+    /** Number of samples */
+    CUDA_HOSTDEV
+    constexpr
+    size_t size() const;
+
+    /**
+     * Return a sub-Linspace over the half-open interval [start, stop).
+     *
+     * The behavior is undefined if [start, stop) is not a valid interval
+     * (i.e. start > stop) or if start or stop are out of range
+     * (i.e. if start >= size() or stop > size()).
+     *
+     * @param[in] start start position
+     * @param[in] stop end position (not included in interval)
+     */
+    CUDA_HOSTDEV
+    Linspace<T> subinterval(size_t start, size_t stop) const;
+
+    /** Check if the sequence contains no samples. */
+    CUDA_HOSTDEV
+    constexpr
+    bool empty() const;
+
+private:
+    T _first;
+    T _spacing;
+    size_t _size;
+};
+
+template<typename T, typename U>
+CUDA_HOSTDEV
+bool operator==(const Linspace<T> &, const Linspace<U> &);
+
+template<typename T, typename U>
+CUDA_HOSTDEV
+bool operator!=(const Linspace<T> &, const Linspace<U> &);
+
+/**
+ * Return the position where the specified value would be inserted in the
+ * sequence in order to maintain sorted order.
+ */
+template<typename T, typename U>
+CUDA_HOSTDEV
+size_t where(const Linspace<T> & x, U val);
+
+}}
+
+#define ISCE_CORE_LINSPACE_ICC
+#include "Linspace.icc"
+#undef ISCE_CORE_LINSPACE_ICC
+
+#endif
+
