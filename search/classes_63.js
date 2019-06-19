15aca44d1118825c0c2283b341d9dc0b64e0583c
gmanipon <gmanipon@jpl.nasa.gov>
2019-06-19 18:48:55 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/PR-isce-develop/334/)

--- search/classes_63.js
+++ search/classes_63.js
@@ -1,12 +1,10 @@
 var searchData=
 [
   ['cannyedgedetector',['CannyEdgeDetector',['../classCannyEdgeDetector.html',1,'']]],
-  ['cea',['CEA',['../structisce_1_1cuda_1_1core_1_1CEA.html',1,'isce::cuda::core']]],
   ['cea',['CEA',['../structisce_1_1core_1_1CEA.html',1,'isce::core']]],
   ['changedetector',['ChangeDetector',['../classChangeDetector.html',1,'']]],
   ['correlator',['Correlator',['../classampcor_1_1correlators_1_1Correlator.html',1,'ampcor::correlators']]],
   ['covariance',['Covariance',['../classisce_1_1signal_1_1Covariance.html',1,'isce::signal']]],
   ['crossmul',['Crossmul',['../classisce_1_1signal_1_1Crossmul.html',1,'isce::signal']]],
-  ['cube',['Cube',['../classisce_1_1core_1_1Cube.html',1,'isce::core']]],
-  ['cudaerror',['CudaError',['../structisce_1_1cuda_1_1except_1_1CudaError.html',1,'isce::cuda::except']]]
+  ['cube',['Cube',['../classisce_1_1core_1_1Cube.html',1,'isce::core']]]
 ];
