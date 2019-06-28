6f200864ba9978618ee7b56d59246bc9637316d7
gmanipon <gmanipon@jpl.nasa.gov>
2019-06-28 22:22:20 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/PR-isce-develop/370/)

--- search/variables_65.js
+++ search/variables_65.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['eartheccentricitysquared',['EarthEccentricitySquared',['../namespaceisce_1_1core.html#ac44534ec97ee0514df2e8215d3706ddc',1,'isce::core']]],
   ['earthsemimajoraxis',['EarthSemiMajorAxis',['../namespaceisce_1_1core.html#ab458d4a8610f8eea4267c4e0b0e028f3',1,'isce::core']]],
-  ['ellipse',['ellipse',['../structisce_1_1core_1_1ProjectionBase.html#a660995c808f7d48dab3b3f81228ecedd',1,'isce::core::ProjectionBase::ellipse()'],['../structisce_1_1cuda_1_1core_1_1ProjectionBase.html#af66f05de3789d191ce3c709f18e908c9',1,'isce::cuda::core::ProjectionBase::ellipse()']]],
+  ['ellipse',['ellipse',['../structisce_1_1core_1_1ProjectionBase.html#a660995c808f7d48dab3b3f81228ecedd',1,'isce::core::ProjectionBase']]],
   ['epochs',['epochs',['../structisce_1_1core_1_1Orbit.html#abcbcc6a63657e37eddc1cd43ed104c10',1,'isce::core::Orbit']]]
 ];
