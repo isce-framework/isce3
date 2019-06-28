c5c419181b33fa0dbac697f6e5ffb3f7a938fd55
gmanipon <gmanipon@jpl.nasa.gov>
2019-06-28 18:07:05 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/PR-isce-develop/367/)

--- search/classes_65.js
+++ search/classes_65.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['ellipsoid',['Ellipsoid',['../classisce_1_1core_1_1Ellipsoid.html',1,'isce::core']]],
   ['error',['Error',['../structisce_1_1except_1_1Error.html',1,'isce::except']]],
-  ['eulerangles',['EulerAngles',['../classisce_1_1core_1_1EulerAngles.html',1,'isce::core']]],
-  ['event',['Event',['../classisce_1_1cuda_1_1core_1_1Event.html',1,'isce::cuda::core']]]
+  ['eulerangles',['EulerAngles',['../classisce_1_1core_1_1EulerAngles.html',1,'isce::core']]]
 ];
--- search/namespaces_69.js
+++ search/namespaces_69.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['core',['core',['../namespaceisce_1_1core.html',1,'isce']]],
-  ['cuda',['cuda',['../namespaceisce_1_1cuda.html',1,'isce']]],
   ['except',['except',['../namespaceisce_1_1except.html',1,'isce']]],
   ['fftw3cxx',['fftw3cxx',['../namespaceisce_1_1fftw3cxx.html',1,'isce']]],
   ['geometry',['geometry',['../namespaceisce_1_1geometry.html',1,'isce']]],
