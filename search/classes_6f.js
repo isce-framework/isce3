01612440521451c39922e07ad6a3e794b87879c1
gmanipon <gmanipon@jpl.nasa.gov>
2019-07-01 22:53:42 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/isce-develop/213/)

--- search/classes_6f.js
+++ search/classes_6f.js
@@ -1,9 +1,7 @@
 var searchData=
 [
-  ['orbit',['Orbit',['../structisce_1_1core_1_1Orbit.html',1,'isce::core']]],
   ['orbit',['Orbit',['../classisce_1_1orbit__wip_1_1Orbit.html',1,'isce::orbit_wip']]],
-  ['orbit',['Orbit',['../classisce_1_1cuda_1_1orbit__wip_1_1Orbit.html',1,'isce::cuda::orbit_wip']]],
-  ['orbitpoint',['OrbitPoint',['../structisce_1_1cuda_1_1orbit__wip_1_1OrbitPoint.html',1,'isce::cuda::orbit_wip']]],
+  ['orbit',['Orbit',['../structisce_1_1core_1_1Orbit.html',1,'isce::core']]],
   ['orbitpoint',['OrbitPoint',['../structisce_1_1orbit__wip_1_1OrbitPoint.html',1,'isce::orbit_wip']]],
   ['orbitview',['OrbitView',['../classisce_1_1orbit__wip_1_1OrbitView.html',1,'isce::orbit_wip']]]
 ];
