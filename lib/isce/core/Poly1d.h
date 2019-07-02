e7b946fd6f9ec521887aa0776733618b979df710
Ryan Burns <rtburns@jpl.nasa.gov>
2019-07-02 15:19:49 -0700

    Set all Poly1d members public

--- lib/isce/core/Poly1d.h
+++ lib/isce/core/Poly1d.h
@@ -23,7 +23,7 @@
  * where \f$a_i\f$ represents the coefficients, \f$\mu\f$ represents the mean and 
  * \f$\sigma\f$ represents the norm*/
 class isce::core::Poly1d {
-private:
+public:
 
     /** Order of the polynomial */
     int order;
@@ -33,8 +33,6 @@ private:
     double norm;
     /** Coefficients of the polynomial */
     std::vector<double> coeffs;
-
-public:
 
     /** Constructor
      *
