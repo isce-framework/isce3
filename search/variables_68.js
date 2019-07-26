f46400e3c706ee262854fc69efd86387c2ce8325
gmanipon <gmanipon@jpl.nasa.gov>
2019-07-26 00:25:16 +0000

    auto update of docs (https://nisar-adt-ci.jpl.nasa.gov/job/PR-isce-develop/404/)

--- search/variables_68.js
+++ search/variables_68.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hours',['hours',['../structisce_1_1core_1_1TimeDelta.html#a37663106e7d9f7410971705084e68814',1,'isce::core::TimeDelta']]]
+  ['hours',['hours',['../classisce_1_1core_1_1TimeDelta.html#a37663106e7d9f7410971705084e68814',1,'isce::core::TimeDelta']]]
 ];
--- search/variables_6d.js
+++ search/variables_6d.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['mat',['mat',['../structisce_1_1core_1_1Pegtrans.html#aa9c4c83023ab8070cac394c7d640acd2',1,'isce::core::Pegtrans']]],
   ['matinv',['matinv',['../structisce_1_1core_1_1Pegtrans.html#ad5ed255a751bdade332b89e9556f9024',1,'isce::core::Pegtrans']]],
-  ['mean',['mean',['../structisce_1_1core_1_1Poly1d.html#a5d1232ac38aa195ef04b93cab1009f17',1,'isce::core::Poly1d']]],
-  ['minutes',['minutes',['../structisce_1_1core_1_1TimeDelta.html#afcb9408ade4e45614a4c0f08487a6c06',1,'isce::core::TimeDelta']]]
+  ['mean',['mean',['../classisce_1_1core_1_1Poly1d.html#a5d1232ac38aa195ef04b93cab1009f17',1,'isce::core::Poly1d']]],
+  ['minutes',['minutes',['../classisce_1_1core_1_1TimeDelta.html#afcb9408ade4e45614a4c0f08487a6c06',1,'isce::core::TimeDelta']]]
 ];
