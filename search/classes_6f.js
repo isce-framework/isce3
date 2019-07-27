cb84049b8ec8afeccc8720d4d1bbc755812f4848
gmanipon <gmanipon@jpl.nasa.gov>
2019-07-27 09:47:40 +0000

    auto update of docs (https://nisar-ci.jpl.nasa.gov/job/isce-develop-nightly-build/320/)

--- search/classes_6f.js
+++ search/classes_6f.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['orbit',['Orbit',['../classisce_1_1orbit__wip_1_1Orbit.html',1,'isce::orbit_wip']]],
-  ['orbit',['Orbit',['../classisce_1_1core_1_1Orbit.html',1,'isce::core']]],
+  ['orbit',['Orbit',['../structisce_1_1core_1_1Orbit.html',1,'isce::core']]],
   ['orbitpoint',['OrbitPoint',['../structisce_1_1orbit__wip_1_1OrbitPoint.html',1,'isce::orbit_wip']]],
   ['orbitview',['OrbitView',['../classisce_1_1orbit__wip_1_1OrbitView.html',1,'isce::orbit_wip']]]
 ];
--- search/functions_61.js
+++ search/functions_61.js
@@ -7,7 +7,7 @@ var searchData=
   ['addbandtovrt',['addBandToVRT',['../classisce_1_1io_1_1Raster.html#a734742e9b608df12762056d77fe3deff',1,'isce::io::Raster']]],
   ['addrastertovrt',['addRasterToVRT',['../classisce_1_1io_1_1Raster.html#a2c9aec0bb00bd4e050cfea2d80becbea',1,'isce::io::Raster']]],
   ['addrawbandtovrt',['addRawBandToVRT',['../classisce_1_1io_1_1Raster.html#a5246c985135e1b88d47b1d85c3e0b27b',1,'isce::io::Raster']]],
-  ['addstatevector',['addStateVector',['../classisce_1_1core_1_1Orbit.html#ab318bf67c0e254c989e6ce985b409238',1,'isce::core::Orbit']]],
+  ['addstatevector',['addStateVector',['../structisce_1_1core_1_1Orbit.html#ab318bf67c0e254c989e6ce985b409238',1,'isce::core::Orbit']]],
   ['arange',['arange',['../namespaceisce_1_1core.html#a607ae942e885db44bc22f28d80a82b39',1,'isce::core']]],
   ['attitude',['attitude',['../classisce_1_1product_1_1Metadata.html#a0ab5bc4a70a1a9545d49c4cb4e2e66eb',1,'isce::product::Metadata::attitude() const '],['../classisce_1_1product_1_1Metadata.html#a207f3cfcf5e20a60fb3edb830bfa779a',1,'isce::product::Metadata::attitude()'],['../classisce_1_1product_1_1Metadata.html#aa5c20f7f3511d867f409d1a1fceceb2e',1,'isce::product::Metadata::attitude(const isce::core::EulerAngles &amp;att)'],['../classisce_1_1core_1_1Attitude.html#a985117dc9ff5d3afdc0fec35b56cd313',1,'isce::core::Attitude::Attitude()']]],
   ['attitudetype',['attitudeType',['../classisce_1_1core_1_1Attitude.html#aa6a0519af6d98cd56a7db75278eb9791',1,'isce::core::Attitude']]],
