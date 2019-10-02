c4f00f8f9b6f92ca0100d956e457ed6c2357a92d
Geoffrey M Gunter <geoffrey.m.gunter@jpl.nasa.gov>
2019-10-02 14:46:30 -0700

    Make style changes to geometry
     - Move docstrings from implementation file to header file
     - Remove extra indentation within namespace scope
     - Wrap long docstring lines
     - Change cartesian_t -> Vec3

--- lib/isce/geometry/geometry.h
+++ lib/isce/geometry/geometry.h
@@ -10,74 +10,173 @@
  * There are no classes defined in this file. Its a collection of functions
  * that are meant to be light weight versions of isce::geometry::Topo and
  * isce::geometry::Geo2rdr.*/
+
 #pragma once
 
 #include "forward.h"
-#include <isce/core/forward.h>
-#include <isce/product/forward.h>
 
+#include <isce/core/forward.h>
 #include <isce/core/Constants.h>
+
+#include <isce/product/forward.h>
 
 // Declaration
 namespace isce {
-    //! The isce::geometry namespace
-    namespace geometry {
+//! The isce::geometry namespace
+namespace geometry {
 
-        // Some useful typedefs from isce::core
-        typedef isce::core::Vec3 cartesian_t;
-        typedef isce::core::cartmat_t cartmat_t;
+/**
+ * Radar geometry coordinates to map coordinates transformer
+ *
+ * This is meant to be the light version of isce::geometry::Topo and not meant
+ * to be used for processing large number of targets of interest. Note that
+ * doppler and wavelength are meant for completeness and this method can be
+ * used with both Native and Zero Doppler geometries. For details of the
+ * algorithm, see the \ref overview_geometry "geometry overview".
+ *
+ * @param[in] aztime azimuth time corresponding to line of interest
+ * @param[in] slantRange slant range corresponding to pixel of interest
+ * @param[in] doppler doppler model value corresponding to line,pixel
+ * @param[in] orbit Orbit object
+ * @param[in] ellipsoid Ellipsoid object
+ * @param[in] demInterp DEMInterpolator object
+ * @param[out] targetLLH output Lon/Lat/Hae corresponding to aztime and slantRange
+ * @param[in] wvl imaging wavelength
+ * @param[in] side +1 for left and -1 for right
+ * @param[in] threshold Distance threshold for convergence
+ * @param[in] maxIter Number of primary iterations
+ * @param[in] extraIter Number of secondary iterations
+ * @param[in] orbitMethod Orbit interpolation method
+ */
+int rdr2geo(double aztime, double slantRange, double doppler,
+            const isce::core::Orbit & orbit,
+            const isce::core::Ellipsoid & ellipsoid,
+            const DEMInterpolator & demInterp,
+            isce::core::Vec3 & targetLLH,
+            double wvl, int side, double threshold, int maxIter, int extraIter,
+            isce::core::orbitInterpMethod orbitMethod);
 
-        /** Radar geometry coordinates to map coordinates transformer*/
-        int rdr2geo(double, double, double,
-                    const isce::core::Orbit &,
-                    const isce::core::Ellipsoid &,
-                    const DEMInterpolator &,
-                    cartesian_t &,
-                    double, int, double, int, int,
-                    isce::core::orbitInterpMethod);
+/**
+ * Radar geometry coordinates to map coordinates transformer
+ *
+ * This is the elementary transformation from radar geometry to map
+ * geometry. The transformation is applicable for a single slant range
+ * and azimuth time (i.e., a single point target). The slant range and
+ * Doppler information are encapsulated in the Pixel object, so this
+ * function can work for both zero and native Doppler geometries. The
+ * azimuth time information is encapsulated in the TCNbasis and
+ * StateVector of the platform. For algorithmic details, see
+ * \ref overview_geometry "geometry overview".
+ *
+ * @param[in] pixel Pixel object
+ * @param[in] TCNbasis Geocentric TCN basis corresponding to pixel
+ * @param[in] pos/vel position and velocity as Vec3 objects
+ * @param[in] ellipsoid Ellipsoid object
+ * @param[in] demInterp DEMInterpolator object
+ * @param[out] targetLLH output Lon/Lat/Hae corresponding to pixel
+ * @param[in] side +1 for left and -1 for right
+ * @param[in] threshold Distance threshold for convergence
+ * @param[in] maxIter Number of primary iterations
+ * @param[in] extraIter Number of secondary iterations
+ */
+int rdr2geo(const isce::core::Pixel & pixel,
+            const isce::core::Basis & TCNbasis,
+            const isce::core::Vec3& pos,
+            const isce::core::Vec3& vel,
+            const isce::core::Ellipsoid & ellipsoid,
+            const DEMInterpolator & demInterp,
+            isce::core::Vec3 & targetLLH,
+            int side, double threshold, int maxIter, int extraIter);
 
-        /** Radar geometry coordinates to map coordinates transformer*/
-        int rdr2geo(const isce::core::Pixel &,
-                    const isce::core::Basis &,
-                    const isce::core::Vec3& pos,
-                    const isce::core::Vec3& vel,
-                    const isce::core::Ellipsoid &,
-                    const DEMInterpolator &,
-                    cartesian_t &,
-                    int, double, int, int);
+/**
+ * Map coordinates to radar geometry coordinates transformer
+ *
+ * This is the elementary transformation from map geometry to radar geometry.
+ * The transformation is applicable for a single lon/lat/h coordinate (i.e., a
+ * single point target). For algorithmic details,
+ * see \ref overview_geometry "geometry overview".
+ *
+ * @param[in] inputLLH Lon/Lat/Hae of target of interest
+ * @param[in] ellipsoid Ellipsoid object
+ * @param[in] orbit Orbit object
+ * @param[in] doppler   Poly2D Doppler model
+ * @param[out] aztime azimuth time of inputLLH w.r.t reference epoch of the orbit
+ * @param[out] slantRange slant range to inputLLH
+ * @param[in] wavelength Radar wavelength
+ * @param[in] startingRange Starting slant range of reference image
+ * @param[in] rangePixelSpacing Slant range pixel spacing
+ * @param[in] rwidth Width (number of samples) of reference image
+ * @param[in] threshold azimuth time convergence threshold in seconds
+ * @param[in] maxIter Maximum number of Newton-Raphson iterations
+ * @param[in] deltaRange step size used for computing derivative of doppler
+ */
+int geo2rdr(const isce::core::Vec3 & inputLLH,
+            const isce::core::Ellipsoid & ellipsoid,
+            const isce::core::Orbit & orbit,
+            const isce::core::Poly2d & doppler,
+            double & aztime, double & slantRange,
+            double wavelength, double startingRange, double rangePixelSpacing, size_t rwidth,
+            double threshold, int maxIter, double deltaRange);
 
-        /** Map coordinates to radar geometry coordinates transformer*/
-        int geo2rdr(const cartesian_t &,
-                    const isce::core::Ellipsoid &,
-                    const isce::core::Orbit &,
-                    const isce::core::Poly2d &,
-                    double &, double &,
-                    double, double, double, size_t,
-                    double, int, double);
+/**
+ * Map coordinates to radar geometry coordinates transformer
+ *
+ * This is the elementary transformation from map geometry to radar geometry.
+ * The transformation is applicable for a single lon/lat/h coordinate (i.e.,
+ * a single point target). For algorithmic details,
+ * see \ref overview_geometry "geometry overview".
+ *
+ * @param[in] inputLLH    Lon/Lat/Hae of target of interest
+ * @param[in] ellipsoid   Ellipsoid object
+ * @param[in] orbit       Orbit object
+ * @param[in] doppler     LUT2d Doppler model
+ * @param[out] aztime     azimuth time of inputLLH w.r.t reference epoch of the orbit
+ * @param[out] slantRange slant range to inputLLH
+ * @param[in] wavelength  Radar wavelength
+ * @param[in] threshold   azimuth time convergence threshold in seconds
+ * @param[in] maxIter     Maximum number of Newton-Raphson iterations
+ * @param[in] deltaRange  step size used for computing derivative of doppler
+ */
+int geo2rdr(const isce::core::Vec3 & inputLLH,
+            const isce::core::Ellipsoid & ellipsoid,
+            const isce::core::Orbit & orbit,
+            const isce::core::LUT2d<double> & doppler,
+            double & aztime, double & slantRange,
+            double wavelength, double threshold, int maxIter, double deltaRange);
 
-        /** Map coordinates to radar geometry coordinates transformer */
-        int geo2rdr(const cartesian_t &,
-                    const isce::core::Ellipsoid &,
-                    const isce::core::Orbit &,
-                    const isce::core::LUT2d<double> &,
-                    double &, double &,
-                    double, double, int, double);
+/**
+ * Utility function to compute geographic bounds for a radar grid
+ *
+ * @param[in] orbit     Orbit object
+ * @param[in] ellipsoid Ellipsoid object
+ * @param[in] doppler   LUT2d doppler object
+ * @param[in] lookSide  +1 for left-looking and -1 for right-looking
+ * @param[in] radarGrid RadarGridParameters object
+ * @param[in] xoff      Column index of radar subwindow
+ * @param[in] yoff      Row index of radar subwindow
+ * @param[in] xsize     Number of columns of radar subwindow
+ * @param[in] ysize     Number of rows of radar subwindiw
+ * @param[in] margin    Padding of extracted DEM (radians)
+ * @param[out] min_lon  Minimum longitude of geographic region (radians)
+ * @param[out] min_lat  Minimum latitude of geographic region (radians)
+ * @param[out] max_lon  Maximum longitude of geographic region (radians)
+ * @param[out] max_lat  Maximum latitude of geographic region (radians)
+ */
+void computeDEMBounds(const isce::core::Orbit & orbit,
+                      const isce::core::Ellipsoid & ellipsoid,
+                      const isce::core::LUT2d<double> & doppler,
+                      int lookSide,
+                      const isce::product::RadarGridParameters & radarGrid,
+                      size_t xoff,
+                      size_t yoff,
+                      size_t xsize,
+                      size_t ysize,
+                      double margin,
+                      double & min_lon,
+                      double & min_lat,
+                      double & max_lon,
+                      double & max_lat);
 
-        /** Utility function to compute geographic bounds for a radar grid */
-        void computeDEMBounds(const isce::core::Orbit & orbit,
-                              const isce::core::Ellipsoid & ellipsoid,
-                              const isce::core::LUT2d<double> & doppler,
-                              int lookSide,
-                              const isce::product::RadarGridParameters & radarGrid,
-                              size_t xoff,
-                              size_t yoff,
-                              size_t xsize,
-                              size_t ysize,
-                              double margin,
-                              double & min_lon,
-                              double & min_lat,
-                              double & max_lon,
-                              double & max_lat);
-
-    }
 }
+}
+
