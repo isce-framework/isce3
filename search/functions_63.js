dab134a1c56016b703c182d6e9e5580644019cff
gmanipon <gmanipon@jpl.nasa.gov>
2019-10-29 18:57:40 +0000

    auto update of docs (https://nisar-adt-ci.jpl.nasa.gov/job/PR-isce-develop/594/)

--- search/functions_63.js
+++ search/functions_63.js
@@ -12,7 +12,6 @@ var searchData=
   ['commonazimuthbandwidth',['commonAzimuthBandwidth',['../classisce_1_1signal_1_1Crossmul.html#a4cfc97b9e81a68ba948760534fb8ddab',1,'isce::signal::Crossmul']]],
   ['comparecomplex',['compareComplex',['../namespaceisce_1_1core.html#af5eda98731f7f62fce5c7f428f0e61a5',1,'isce::core']]],
   ['comparefloatingpoint',['compareFloatingPoint',['../namespaceisce_1_1core.html#ac18b3e621f399811a3e54baafd30c8f2',1,'isce::core']]],
-  ['computeacceleration',['computeAcceleration',['../classisce_1_1core_1_1Orbit.html#a1f03ba5b201db6e285c7a43297ff8609',1,'isce::core::Orbit']]],
   ['computebaselines',['computeBaselines',['../classisce_1_1core_1_1Baseline.html#a0bb7d20662852a43c380d99977f82d64',1,'isce::core::Baseline']]],
   ['computedembounds',['computeDEMBounds',['../namespaceisce_1_1geometry.html#a5ec18a9486f88ef4a91ea670404afe0e',1,'isce::geometry']]],
   ['computeheightstats',['computeHeightStats',['../classisce_1_1geometry_1_1DEMInterpolator.html#a5d5b2ce2150dd07a95999ea44fc357bc',1,'isce::geometry::DEMInterpolator']]],
