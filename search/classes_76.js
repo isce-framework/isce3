6bf42e5a132a51fe45edc34c2417f954941d5e73
gmanipon <gmanipon@jpl.nasa.gov>
2019-11-20 18:50:34 +0000

    auto update of docs (https://nisar-adt-ci.jpl.nasa.gov/job/PR-isce-develop/642/)

---
+++ search/classes_76.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['vector',['Vector',['../classisce_1_1core_1_1Vector.html',1,'isce::core']]],
+  ['vector_3c_203_20_3e',['Vector&lt; 3 &gt;',['../classisce_1_1core_1_1Vector.html',1,'isce::core']]]
+];
---
+++ search/functions_71.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['quaternion',['Quaternion',['../classisce_1_1core_1_1Quaternion.html#a74fbcd628b5f40adb469eee52fb99adf',1,'isce::core::Quaternion::Quaternion()'],['../classisce_1_1core_1_1Quaternion.html#af03a7aa420f0e7a080a3cd205cf4accc',1,'isce::core::Quaternion::Quaternion(const std::vector&lt; double &gt; &amp;time, const std::vector&lt; double &gt; &amp;quaternions)']]],
+  ['qvec',['qvec',['../classisce_1_1core_1_1Quaternion.html#a636b5b7edd81e359d74c34612336e5b6',1,'isce::core::Quaternion']]]
+];
