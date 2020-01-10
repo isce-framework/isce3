a0c81d44630bd8193c9146683adbafe857f5aad8
gmanipon <gmanipon@jpl.nasa.gov>
2020-01-10 16:11:42 +0000

    auto update of docs (https://nisar-adt-ci.jpl.nasa.gov/job/PR-isce-develop/800/)

--- search/functions_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['knabkernel',['KnabKernel',['../classisce_1_1core_1_1KnabKernel.html#a9ba159b19ea129f7af8d012dc4438ec7',1,'isce::core::KnabKernel']]]
-];
--- search/typedefs_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['triangle',['Triangle',['../namespaceisce_1_1geometry.html#a26a60f8bd02b6dd531d45480b5a3afbf',1,'isce::geometry']]]
-];
