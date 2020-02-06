c4a5bdd054893cf254ae3aa5c46bed11805327df
Ryan Burns <rtburns-jpl@users.noreply.github.com>
2020-02-06 13:59:49 -0800

    Add simple test to demonstrate usage

--- tests/python/extensions/pybind_isce3/core/orbit.py
+++ tests/python/extensions/pybind_isce3/core/orbit.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python3
 
 import numpy.testing as npt
-import pybind_isce3 as isce
 
-def test_load_h5():
+def load_h5():
+    from pybind_isce3.core import Orbit
     from iscetest import data
     from os import path
     f = path.join(data, "envisat.h5")
-    o = isce.core.Orbit.load_from_h5(f, "/science/LSAR/SLC/metadata/orbit")
+    return Orbit.load_from_h5(f, "/science/LSAR/SLC/metadata/orbit")
 
-    o.time_at(0)
-    o.position_at(0)
-    o.velocity_at(0)
+o = load_h5();
+
+# Test that accessors exist
+def test_props():
+    o = load_h5();
+    o.position
+    o.velocity
+
+# Test that loaded data is valid
+def test_members():
+    import numpy
+    from numpy.linalg import norm
+    o = load_h5();
+
+    # Check valid earth orbit distance
+    earth_radius  =  6_000e3 # meters
+    geostationary = 35_000e3 # meters
+    altitude = numpy.array([norm(pos) for pos in o.position])
+    assert(all(altitude > earth_radius))
+    assert(all(altitude < geostationary))
+
+    # Check valid orbital velocity
+    # How fast should a satellite move?
+    # Probably faster than a car, but slower than the speed of light.
+    car = 100 * 1000./3600. # km/h to m/s
+    light = 3e8
+    velocity = numpy.array([norm(vel) for vel in o.velocity])
+    assert(all(velocity > car))
+    assert(all(velocity < light))
