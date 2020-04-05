3621df75af61b02d97de9948a78c425b4aeeabf5
Brian Hawkins <Brian.P.Hawkins@jpl.nasa.gov>
2020-04-05 00:22:29 -0700

    Add vectorized interp1d.

--- python/extensions/pybind_isce3/core/Interp1d.cpp
+++ python/extensions/pybind_isce3/core/Interp1d.cpp
@@ -1,6 +1,8 @@
 #include "Interp1d.h"
 #include <pybind11/complex.h>
 #include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+#include <valarray>
 #include <isce/core/Interp1d.h>
 #include <isce/core/Kernels.h>
 #include <isce/except/Error.h>
@@ -11,31 +13,46 @@ using namespace isce::core;
 using isce::except::RuntimeError;
 
 template <typename TK, typename TD>
-static TD interp(const Kernel<TK> & kernel, py::buffer_info & info, double t)
+static py::object
+interp(const Kernel<TK> & kernel, py::buffer_info & info, py::object t)
 {
     TD * data = static_cast<TD *>(info.ptr);
     int stride = info.strides[0] / sizeof(TD);
-    return interp1d(kernel, data, info.shape[0], stride, t);
+    auto n = info.shape[0];
+    if (py::isinstance<py::float_>(t)) {
+        return py::cast(interp1d(kernel, data, n, stride, py::float_(t)));
+    }
+    else if (py::isinstance<py::array_t<double>>(t)) {
+        auto ta = py::array_t<double>(t).unchecked<1>();
+        std::valarray<TD> out(ta.size());
+        #pragma omp parallel for
+        for (size_t i=0; i < ta.size(); ++i) {
+            out[i] = interp1d(kernel, data, n, stride, ta(i));
+        }
+        return py::cast(out, py::return_value_policy::take_ownership);
+    }
+    throw RuntimeError(ISCE_SRCINFO(),
+        "interp1d expects time is float or numpy.float64 array");
 }
 
 template <typename T>
 void addbinding_interp1d(py::module & m, const char * name)
 {
-    m.def(name, [](Kernel<T> & kernel, py::buffer buf, double t) {
+    m.def(name, [](Kernel<T> & kernel, py::buffer buf, py::object t) {
         py::buffer_info info = buf.request();
         using CT = std::complex<T>;
         if (info.ndim != 1) {
-            throw RuntimeError(ISCE_SRCINFO(), "buffer object must be 1-D");
+            throw RuntimeError(ISCE_SRCINFO(), "data buffer must be 1-D");
         }
         if (info.format == py::format_descriptor<float>::format()) {
-            return py::cast(interp<T,float>(kernel, info, t));
+            return interp<T,float>(kernel, info, t);
         }
         else if (info.format == py::format_descriptor<double>::format()) {
-            return py::cast(interp<T,double>(kernel, info, t));
+            return interp<T,double>(kernel, info, t);
         }
         else if (info.format == py::format_descriptor<CT>::format()) {
             // NOTE: not mixing float and double to avoid weird promotion rules
-            return py::cast(interp<T,CT>(kernel, info, t));
+            return interp<T,CT>(kernel, info, t);
         }
         throw RuntimeError(ISCE_SRCINFO(), "Unsupported types for interp1d");
     });
