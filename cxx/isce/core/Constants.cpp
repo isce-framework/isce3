d13de36568daa71d88de93206546d969fcff61ba
Brian Hawkins <Brian.P.Hawkins@jpl.nasa.gov>
2020-04-13 00:31:23 -0700

    Ditch std::map and remove duplicate code in LUT2d binding.

--- cxx/isce/core/Constants.cpp
+++ cxx/isce/core/Constants.cpp
@@ -14,18 +14,13 @@ parseDataInterpMethod(const std::string & method)
     std::transform(m.begin(), m.end(), m.begin(),
         [](unsigned char c) { return std::tolower(c); });
 
-    std::map<std::string, dataInterpMethod> methods {
-        {"sinc",      SINC_METHOD},
-        {"bilinear",  BILINEAR_METHOD},
-        {"bicubic",   BICUBIC_METHOD},
-        {"nearest",   NEAREST_METHOD},
-        {"biquintic", BIQUINTIC_METHOD}
-    };
-    auto it = methods.find(m);
-    if (it == methods.end())
-        throw InvalidArgument(ISCE_SRCINFO(), "Unknown interp method");
+    if (m == "sinc")      return SINC_METHOD;
+    if (m == "bilinear")  return BILINEAR_METHOD;
+    if (m == "bicubic")   return BICUBIC_METHOD;
+    if (m == "nearest")   return NEAREST_METHOD;
+    if (m == "biquintic") return BIQUINTIC_METHOD;
 
-    return it->second;
+    throw InvalidArgument(ISCE_SRCINFO(), "Unknown interp method");
 }
 
 }} // isce::core
