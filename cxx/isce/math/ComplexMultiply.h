4313cd3a9dc142bd3587d2b03171580e1d2b6803
Brian Hawkins <Brian.P.Hawkins@jpl.nasa.gov>
2020-04-16 23:50:04 -0700

    Inline the new complex operator* overloads.

--- cxx/isce/math/ComplexMultiply.h
+++ cxx/isce/math/ComplexMultiply.h
@@ -5,11 +5,11 @@
 /** Promotion rules so T * complex<U> works for {T,U} in {float,double}. */
 namespace isce { namespace math { namespace complex_multiply {
 
-auto operator*(float t, std::complex<double> u) {
+inline auto operator*(float t, std::complex<double> u) {
     return double(t) * u;
 }
 
-auto operator*(std::complex<double> t, float u) {
+inline auto operator*(std::complex<double> t, float u) {
     return t * double(u);
 }
 
