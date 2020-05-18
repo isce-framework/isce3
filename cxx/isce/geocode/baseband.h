4db08c28829468cef744c141b1631cf6c6b9f874
Heresh Fattahi <heresh.fattahi@jpl.nasa.gov>
2020-05-18 14:23:57 -0700

    Update cxx/isce/geocode/baseband.h
    fix doc for baseband
    
    Co-Authored-By: Geoffrey M Gunter <geoffrey.m.gunter@jpl.nasa.gov>

--- cxx/isce/geocode/baseband.h
+++ cxx/isce/geocode/baseband.h
@@ -8,7 +8,7 @@
 
 namespace isce { namespace geocode {
     /**
-     * param[in] data a matrix of data that needs to be sub-banded in azimuth direction
+     * param[in,out] data a matrix of data that needs to be sub-banded in azimuth direction
      * param[in] startingRange starting range of the data block
      * param[in] sensingStart  starting azimuth time of the data block
      * param[in] rangePixelSpacing spacing of the slant range
