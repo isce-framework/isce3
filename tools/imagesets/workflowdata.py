0df6dc36f92dc91bd74a07e8c7a08f55fff02fc6
Yang Zheng <yang.zheng@jpl.nasa.gov>
2020-12-04 22:09:53 +0000

    Add more test data and updated test and data names

--- tools/imagesets/workflowdata.py
+++ tools/imagesets/workflowdata.py
@@ -9,9 +9,54 @@ workflowdata = [
             "README.txt",
         ],
     ),
-    # and so forth
+        # and so forth
     (
-        "L1_RSLC_UAVSAR_SanAnd1",
+        "L0B_RRSD_REE2",
+        [
+            "REE_L0B_NISAR_array144sq_01.h5",
+            "REE_ANTPAT_DATA.h5",
+            "README.txt",
+        ],
+    ),    
+    (
+        "L0B_RRSD_DIST1",
+        [
+            "REE_L0B_NISAR_DATA_PASS1.h5",
+            "REE_ANTPAT_DATA.h5",
+            "README.txt",
+        ],
+    ),    
+    (
+        "L0B_RRSD_DIST2",
+        [
+            "REE_L0B_NISAR_DATA_PASS2.h5",
+            "REE_ANTPAT_DATA.h5",
+            "README.txt",
+        ],
+    ),  
+    (
+        "L0B_RRSD_ALPSRP037370690",
+        [
+            "ALPSRP037370690.L0B.h5",  
+            "README.txt",
+        ]
+    ),
+    (
+        "L0B_RRSD_ALPSRP271200680",
+        [
+            "ALPSRP271200680.L0B.h5",  
+            "README.txt",
+        ]
+    ),
+    (
+        "L1_RSLC_S1B_IW_SLC__1SDV_20180504T104507_20180504T104535_010770_013AEE_919F",
+        [
+            "S1B_IW_SLC__1SDV_20180504T104507_20180504T104535_010770_013AEE_919F.h5",
+            "README.txt",
+        ]
+    ),
+    (
+        "L1_RSLC_UAVSAR_SanAnd_05024_18038_006_180730_L090_CX_129_05",
         [
             "SanAnd_05024_18038_006_180730_L090_CX_129_05.h5",
             "nisar-dem/EPSG32610/EPSG32610.vrt",
@@ -20,18 +65,53 @@ workflowdata = [
             "README.txt",
         ],
     ),
+    (
+        "L1_RSLC_UAVSAR_SanAnd_05518_12018_000_120419_L090_CX_143_03",
+        [
+            "SanAnd_05518_12018_000_120419_L090_CX_143_03.h5",
+            "dem.tif",
+            "README.txt",
+        ],
+    ),
+    (
+        "L1_RSLC_UAVSAR_SanAnd_05518_12128_008_121105_L090_CX_143_02",
+        [
+            "SanAnd_05518_12128_008_121105_L090_CX_143_02.h5",
+            "dem.tif",
+            "README.txt",
+        ],
+    ),
+    (
+        "L1_RSLC_UAVSAR_NISARP_32039_19049_005_190717_L090_CX_129_03",
+        [
+            "NISARP_32039_19049_005_190717_L090_CX_129_03.h5",
+            "nisar-dem/EPSG32617/EPSG32617.vrt",
+            "nisar-dem/EPSG32617/N3800E0200.tif",
+            "README.txt",
+        ],
+    ),
 ]
 
 # dictionaries definining mappig of workflow tests to data
 # each key is the test name, value is the corresponding data
 rslctestdict = {
-    "RSLC_REE1": "L0B_RRSD_REE1",
+    "rslc_REE1": "L0B_RRSD_REE1",
     }
     
 gslctestdict = {
-    "GSLC_UAVSAR_SanAnd1": "L1_RSLC_UAVSAR_SanAnd1",
+    "gslc_UAVSAR_SanAnd_05024_18038_006_180730_L090_CX_129_05":
+        "L1_RSLC_UAVSAR_SanAnd_05024_18038_006_180730_L090_CX_129_05",
     }
 
 gcovtestdict = {
-    "GCOV_UAVSAR_SanAnd1": "L1_RSLC_UAVSAR_SanAnd1",
+    "gcov_UAVSAR_SanAnd_05024_18038_006_180730_L090_CX_129_05": 
+        "L1_RSLC_UAVSAR_SanAnd_05024_18038_006_180730_L090_CX_129_05",
+    }
+
+insartestdict = {
+    "insar_UAVSAR_SanAnd_05518_12018-000_12128-008_143": 
+        [
+            "L1_RSLC_UAVSAR_SanAnd_05518_12018_000_120419_L090_CX_143_03",
+            "L1_RSLC_UAVSAR_SanAnd_05518_12128_008_121105_L090_CX_143_02",
+        ]
     }
