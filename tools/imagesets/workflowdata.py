aa061714106c82aee41e9a3d627b76592507c904
Yang Zheng <yang.zheng@jpl.nasa.gov>
2020-12-07 18:13:58 +0000

    Update workflow test data and runconfig files

--- tools/imagesets/workflowdata.py
+++ tools/imagesets/workflowdata.py
@@ -52,6 +52,16 @@ workflowdata = [
         "L1_RSLC_S1B_IW_SLC__1SDV_20180504T104507_20180504T104535_010770_013AEE_919F",
         [
             "S1B_IW_SLC__1SDV_20180504T104507_20180504T104535_010770_013AEE_919F.h5",
+            "nisar-dem/EPSG32718/EPSG32718.vrt",
+            "nisar-dem/EPSG32718/N9000E0000.tif",
+            "nisar-dem/EPSG32718/N9000E0200.tif",
+            "nisar-dem/EPSG32718/N9000E0400.tif",
+            "nisar-dem/EPSG32718/N9200E0000.tif",
+            "nisar-dem/EPSG32718/N9200E0200.tif",
+            "nisar-dem/EPSG32718/N9200E0400.tif",
+            "nisar-dem/EPSG32718/N9400E0000.tif",
+            "nisar-dem/EPSG32718/N9400E0200.tif",
+            "nisar-dem/EPSG32718/N9400E0400.tif",    
             "README.txt",
         ]
     ),
@@ -90,22 +100,55 @@ workflowdata = [
             "README.txt",
         ],
     ),
+    (
+        "L1_RSLC_UAVSAR_NISARP_32039_19052_004_190726_L090_CX_129_02",
+        [
+            "NISARP_32039_19052_004_190726_L090_CX_129_02.h5",
+            "nisar-dem/EPSG32617/EPSG32617.vrt",
+            "nisar-dem/EPSG32617/N3800E0200.tif",
+            "README.txt",
+        ],
+    ),
 ]
 
 # dictionaries definining mappig of workflow tests to data
 # each key is the test name, value is the corresponding data
 rslctestdict = {
     "rslc_REE1": "L0B_RRSD_REE1",
+    #"rslc_REE2": "L0B_RRSD_REE2", # ~1.5 hour test on GPU, QA memory error
+    #"rslc_DIST1": "L0B_RRSD_DIST1",
+    #"rslc_DIST2": "L0B_RRSD_DIST2",
+    #"rslc_ALPSRP037370690": "L0B_RRSD_ALPSRP037370690", # missing "Absolute orbit number"
+    #"rslc_ALPSRP271200680": "L0B_RRSD_ALPSRP271200680", # missing "Absolute orbit number"
     }
     
 gslctestdict = {
     "gslc_UAVSAR_SanAnd_05024_18038_006_180730_L090_CX_129_05":
         "L1_RSLC_UAVSAR_SanAnd_05024_18038_006_180730_L090_CX_129_05",
+    "gslc_UAVSAR_NISARP_32039_19049_005_190717_L090_CX_129_03":  
+        "L1_RSLC_UAVSAR_NISARP_32039_19049_005_190717_L090_CX_129_03",
+    "gslc_UAVSAR_NISARP_32039_19052_004_190726_L090_CX_129_02":  
+        "L1_RSLC_UAVSAR_NISARP_32039_19052_004_190726_L090_CX_129_02",
+    "gslc_UAVSAR_SanAnd_05518_12018_000_120419_L090_CX_143_03":  # QA memory error
+        "L1_RSLC_UAVSAR_SanAnd_05518_12018_000_120419_L090_CX_143_03",
+    "gslc_UAVSAR_SanAnd_05518_12128_008_121105_L090_CX_143_02":  # QA memory error
+        "L1_RSLC_UAVSAR_SanAnd_05518_12128_008_121105_L090_CX_143_02",
     }
 
 gcovtestdict = {
     "gcov_UAVSAR_SanAnd_05024_18038_006_180730_L090_CX_129_05": 
         "L1_RSLC_UAVSAR_SanAnd_05024_18038_006_180730_L090_CX_129_05",
+    "gcov_UAVSAR_NISARP_32039_19049_005_190717_L090_CX_129_03":
+        "L1_RSLC_UAVSAR_NISARP_32039_19049_005_190717_L090_CX_129_03",
+    "gcov_UAVSAR_NISARP_32039_19049_005_190717_L090_CX_129_03":
+        "L1_RSLC_UAVSAR_NISARP_32039_19049_005_190717_L090_CX_129_03",
+    # lowercase polarization error
+    #"gcov_S1B_IW_SLC__1SDV_20180504T104507_20180504T104535_010770_013AEE_919F":
+    #    "L1_RSLC_S1B_IW_SLC__1SDV_20180504T104507_20180504T104535_010770_013AEE_919F",
+    "gcov_UAVSAR_SanAnd_05518_12018_000_120419_L090_CX_143_03":
+        "L1_RSLC_UAVSAR_SanAnd_05518_12018_000_120419_L090_CX_143_03",
+    "gcov_UAVSAR_SanAnd_05518_12128_008_121105_L090_CX_143_02":
+        "L1_RSLC_UAVSAR_SanAnd_05518_12128_008_121105_L090_CX_143_02",
     }
 
 insartestdict = {
@@ -113,5 +156,10 @@ insartestdict = {
         [
             "L1_RSLC_UAVSAR_SanAnd_05518_12018_000_120419_L090_CX_143_03",
             "L1_RSLC_UAVSAR_SanAnd_05518_12128_008_121105_L090_CX_143_02",
-        ]
+        ],
+    "insar_UAVSAR_NISARP_32039_19049-005_19052-004_129":
+        [
+            "L1_RSLC_UAVSAR_NISARP_32039_19049_005_190717_L090_CX_129_03",
+            "L1_RSLC_UAVSAR_NISARP_32039_19052_004_190726_L090_CX_129_02",
+        ],
     }
