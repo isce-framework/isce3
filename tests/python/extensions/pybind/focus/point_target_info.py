20e60f5a7f24a4e7c6a84814173cb679c46309fd
Bo Huang <Bo.Huang@jpl.nasa.gov>
2021-01-25 12:29:29 -0800

    Update tests/python/extensions/pybind/focus/point_target_info.py
    I agree with you Brian.  That was a mistake on my part.
    
    Co-Authored-By: Brian P Hawkins <Brian.P.Hawkins@jpl.nasa.gov>

--- tests/python/extensions/pybind/focus/point_target_info.py
+++ tests/python/extensions/pybind/focus/point_target_info.py
@@ -143,7 +143,7 @@ def find_null_to_null(matched_output, num_nulls_main, num_samples_null, main_pea
             
     #Search for right null
     diffsign_right = np.sign(np.diff(samples_right))
-    if np.any(diffsign_right == -1):
+    if np.any(diffsign_right == 1):
         null_right_idx = first_peak_right_idx + np.where(diffsign_right[:-1] + diffsign_right[1:] == 0)[0][0] + 1
     else:
         null_right_idx = first_peak_right_idx + num_samples_null
