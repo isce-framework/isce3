e3a7e7aea70f79ff2c796ff4a3c6041dd3fd11f0
jungkyoj <jungkyo.jung@jpl.nasa.gov>
2021-09-16 02:46:51 -0700

    update style and documentation

--- python/packages/nisar/workflows/bandpass_insar.py
+++ python/packages/nisar/workflows/bandpass_insar.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import pathlib
 import time
-import os
-
 import h5py
 import journal
 import numpy as np
@@ -16,6 +15,7 @@ from nisar.workflows.yaml_argparse import YamlArgparse
 from nisar.h5 import cp_h5_meta_data
 import nisar.workflows.splitspectrum as splitspectrum
 
+
 def run(cfg: dict):
     '''
     run bandpass
@@ -25,24 +25,25 @@ def run(cfg: dict):
     sec_hdf5 = cfg['InputFileGroup']['SecondaryFilePath']
     freq_pols = cfg['processing']['input_subset']['list_of_frequencies']
     blocksize = cfg['processing']['blocksize']['y']
-
     scratch_path = pathlib.Path(cfg['ProductPathGroup']['ScratchPath'])
 
     # init parameters shared by frequency A and B
     ref_slc = SLC(hdf5file=ref_hdf5)
     sec_slc = SLC(hdf5file=sec_hdf5)
 
-    error_channel = journal.error('bandpass.run')
-    info_channel = journal.info("bandpass.run")
-    info_channel.log("starting bandpass")
+    error_channel = journal.error('bandpass_insar.run')
+    info_channel = journal.info("bandpass_insar.run")
+    info_channel.log("starting bandpass_insar")
 
     t_all = time.time()
 
     # check if bandpass is necessary
-    bandpass_modes = splitspectrum.insar_mixmode(ref_slc, sec_slc, freq_pols)
+    bandpass_modes = splitspectrum.check_insar_mixmode(ref_slc, 
+                     sec_slc, 
+                     freq_pols)
 
     # check if user provided path to raster(s) is a file or directory
-    bandpass_slc_path =  pathlib.Path(f"{scratch_path}/bandpass/")
+    bandpass_slc_path = pathlib.Path(f"{scratch_path}/bandpass/")
 
     if bandpass_modes:
         ref_slc_output = f"{scratch_path}/bandpass/ref_bp.h5"
@@ -53,30 +54,26 @@ def run(cfg: dict):
     
     # freq: [A, B], target : 'ref' or 'sec'
     for freq, target in bandpass_modes.items():
-
-        bandpass = splitspectrum.Splitspectrum()
         pol_list = freq_pols[freq]
 
         # if reference has wide bandwidth, then reference will be bandpassed
         # base   : SLC to be referenced  
         # target : SLC to be bandpassed
         if target == 'ref':
-
-            base_hdf5   = sec_hdf5
+            base_hdf5 = sec_hdf5
             target_hdf5 = ref_hdf5
-            target_slc  = ref_slc
-            base_slc    = sec_slc
+            target_slc = ref_slc
+            base_slc = sec_slc
 
             # update reference SLC path 
             cfg['InputFileGroup']['InputFilePath'] = ref_slc_output
             target_output = ref_slc_output
 
         elif target == 'sec':
-
-            base_hdf5   = ref_hdf5
+            base_hdf5 = ref_hdf5
             target_hdf5 = sec_hdf5
-            target_slc  = sec_slc
-            base_slc    = ref_slc
+            target_slc = sec_slc
+            base_slc = ref_slc
 
             # update secondary SLC path 
             cfg['InputFileGroup']['SecondaryFilePath'] = sec_slc_output
@@ -87,60 +84,74 @@ def run(cfg: dict):
 
         # Copy HDF 5 file to be bandpassed    
         with h5py.File(target_hdf5, 'r', libver='latest', swmr=True) as src_h5, \
-             h5py.File(target_output, 'w', libver='latest', swmr=True) as dst_h5:
+                h5py.File(target_output, 'w', libver='latest', swmr=True) as dst_h5:
             cp_h5_meta_data(src_h5, dst_h5, f'{common_parent_path}')
 
-        ## meta data extraction 
-        base_meta_data = splitspectrum.meta_data_bandpass(base_slc, freq)
-        bandwidth_half = base_meta_data['rg_bandwidth'] * 0.5
-        low_frequency_base = base_meta_data['center_frequency'] - bandwidth_half
-        high_frequency_base = base_meta_data['center_frequency'] + bandwidth_half
+        # meta data extraction 
+        base_meta_data = splitspectrum.get_meta_data_bandpass(base_slc, freq)
+        bandwidth_half = 0.5 * base_meta_data['rg_bandwidth'] 
+        low_frequency_base = base_meta_data['center_frequency'] - \
+                             bandwidth_half
+        high_frequency_base = base_meta_data['center_frequency'] + \
+                              bandwidth_half
 
-        dest_freq_path=f"/science/LSAR/SLC/swaths/frequency{freq}"
+        # initialize bandpass instance
+        bandpass = splitspectrum.SplitSpectrum(target_slc, freq)
+
+        dest_freq_path = f"/science/LSAR/SLC/swaths/frequency{freq}"
         with h5py.File(target_hdf5, 'r', libver='latest', swmr=True) as src_h5, \
-            h5py.File(target_output, 'r+') as dst_h5:
+                h5py.File(target_output, 'r+') as dst_h5:
             for pol in pol_list:
                 
                 target_raster_str = f'HDF5:{target_hdf5}:/{target_slc.slcPath(freq, pol)}'
                 target_slc_raster = isce3.io.Raster(target_raster_str)   
-                cols = target_slc_raster.length         
-                nblocks  = int( cols / blocksize)
+                rows = target_slc_raster.length   
+                cols = target_slc_raster.width         
+                nblocks = int(rows / blocksize)
 
-                bandpass.frame = target_slc
-                bandpass.low_frequency = low_frequency_base
-                bandpass.high_frequency = high_frequency_base
-                bandpass.new_center_frequency = base_meta_data['center_frequency']
-                bandpass.freq = freq
-
-                if (nblocks == 0) :
+                if (nblocks == 0):
                     nblocks = 1
-                elif (np.mod(cols, (nblocks * blocksize)) != 0) :
+                elif (np.mod(rows, (nblocks * blocksize)) != 0):
                     nblocks = nblocks + 1
   
-                for block in range(0,nblocks):
-                    print("-- bandpass block: ",block)
+                for block in range(0, nblocks):
+                    print("-- bandpass block: ", block)
                     row_start = block * blocksize
-                    if ((row_start + blocksize) > cols) :
-                        block_rows_data = cols - row_start
-                    else :
+                    if ((row_start + blocksize) > rows):
+                        block_rows_data = rows - row_start
+                    else:
                         block_rows_data = blocksize
                     
-                    dest_pol_path=f"{dest_freq_path}/{pol}"
-                    target_slc_image = src_h5[dest_pol_path][row_start:row_start + block_rows_data,:]
+                    dest_pol_path = f"{dest_freq_path}/{pol}"
+                    target_slc_image = np.zeros([block_rows_data, cols], 
+                                                dtype=complex)
+
+                    src_h5[dest_pol_path].read_direct(
+                        target_slc_image, 
+                        np.s_[row_start : row_start + block_rows_data, :])
+
                     bandpass.slc_raster = target_slc_image
-                    bandpass_slc, meta = bandpass.bandpass_spectrum()
-      
+                    bandpass_slc, bandpass_meta = bandpass.bandpass_spectrum(
+                        slc_raster=target_slc_image,
+                        low_frequency=low_frequency_base,
+                        high_frequency=high_frequency_base,
+                        new_center_frequency=base_meta_data['center_frequency']
+                        )   
+                      
                     if block == 0:
                         del dst_h5[dest_pol_path]
                         dst_h5.create_dataset(dest_pol_path, 
-                                        np.shape(bandpass_slc), 
-                                        np.complex64,
-                                        chunks=(128, 128),
-                                        data= np.complex64(bandpass_slc),
-                                        maxshape=(None,None))
+                                              np.shape(bandpass_slc), 
+                                              np.complex64,
+                                              chunks=(128, 128),
+                                              data=np.complex64(bandpass_slc),
+                                              maxshape=(rows, bandpass_slc.shape[1]))
                     else:
-                        dst_h5[dest_pol_path].resize((dst_h5[dest_pol_path].shape[0] + bandpass_slc.shape[0]), axis = 0)
-                        dst_h5[dest_pol_path][-bandpass_slc.shape[0]:] = np.complex64(bandpass_slc)
+                        dst_h5[dest_pol_path].resize((dst_h5[dest_pol_path].shape[0] 
+                                                     + bandpass_slc.shape[0]), 
+                                                     axis=0)
+                        dst_h5[dest_pol_path][-bandpass_slc.shape[0] :] = \
+                                                     np.complex64(bandpass_slc)
 
                 dst_h5[dest_pol_path].attrs['description'] = f"Bandpass SLC image ({pol})"
                 dst_h5[dest_pol_path].attrs['units'] = f"DN"
@@ -149,20 +160,21 @@ def run(cfg: dict):
         with h5py.File(target_output, 'r+', libver='latest', swmr=True) as dst_h5:
 
             data = dst_h5[f"{dest_freq_path}/processedCenterFrequency"]
-            data[...] = meta['centerFrequency']
+            data[...] = bandpass_meta['center_frequency']
             data = dst_h5[f"{dest_freq_path}/slantRangeSpacing"]
-            data[...] = meta['rangeSpacing']
+            data[...] = bandpass_meta['range_spacing']
             data = dst_h5[f"{dest_freq_path}/processedRangeBandwidth"]
             data[...] = base_meta_data['rg_bandwidth'] 
             del dst_h5[f"{dest_freq_path}/slantRange"]
-            dst_h5.create_dataset(f"{dest_freq_path}/slantRange",data=meta['slantRange'])
+            dst_h5.create_dataset(f"{dest_freq_path}/slantRange",
+                                  data=bandpass_meta['slant_range'])
 
     # update HDF5 
     if bandpass_modes:
         h5_prep.run(cfg)
 
     t_all_elapsed = time.time() - t_all
-    print ('total processing time: ', t_all_elapsed, ' sec')
+    print('total processing time: ', t_all_elapsed, ' sec')
     info_channel.log(
         f"successfully ran bandpass in {t_all_elapsed:.3f} seconds")
 
--- python/packages/nisar/workflows/splitspectrum.py
+++ python/packages/nisar/workflows/splitspectrum.py
@@ -1,179 +1,240 @@
-#!/usr/bin/env python3
-
 import numpy as np
-from scipy.fft import fft, ifft, fftshift
-from scipy.signal import kaiserord, freqz, firwin, resample
+import journal
+from scipy.fft import fft, ifft
+from scipy.signal import resample
 from numpy.fft import fftfreq
+
 import isce3
 
-def meta_data_bandpass(frame, freq):
+
+def get_meta_data_bandpass(slc_product, freq):
+    """get meta data from SLC object 
+    Parameters
+    ----------
+    slc_product : slc object
+    freq : frequency A or B
+
+    Returns
+    -------
+    meta_data : dict containing meta_data
+    """
     meta_data = dict()
-    rdr_grid         = frame.getRadarGrid(freq)
-    meta_data['rg_pxl_spacing']   = rdr_grid.range_pixel_spacing
-    meta_data['wavelength']      = rdr_grid.wavelength
-    meta_data['rg_sample_freq']   = isce3.core.speed_of_light * 0.5 / meta_data['rg_pxl_spacing']
-    meta_data['rg_bandwidth']     = frame.getSwathMetadata(freq).processed_range_bandwidth
-    meta_data['center_frequency']  = isce3.core.speed_of_light / meta_data['wavelength'] 
-    meta_data['slant_range']      = rdr_grid.slant_range
+    rdr_grid = slc_product.getRadarGrid(freq)
+    meta_data['rg_pxl_spacing'] = rdr_grid.range_pixel_spacing
+    meta_data['wavelength'] = rdr_grid.wavelength
+    meta_data['rg_sample_freq'] = isce3.core.speed_of_light * \
+        0.5 / meta_data['rg_pxl_spacing']
+    meta_data['rg_bandwidth'] = slc_product.getSwathMetadata(
+        freq).processed_range_bandwidth
+    meta_data['center_frequency'] = isce3.core.speed_of_light / \
+        meta_data['wavelength']
+    meta_data['slant_range'] = rdr_grid.slant_range
     return meta_data
 
-def insar_mixmode(ref_slc, sec_slc, pols):
-    """
-    insar_mixmode(ref_slc, sec_slc, pols)
-    check if bandpass is necessary
-    if necessary, determine SLC to be bandpassed
-    ref_slc : reference slc object
-    sec_slc : secondary slc object
-    pol     : polarization list [frequency, polarization]
+
+def check_insar_mixmode(ref_slc, sec_slc, pols):
+    """check if bandpass is needed
+    if necessary, determine which SLC will be bandpassed
+    Parameters
+    ----------
+    ref_slc : reference SLC object
+    sec_slc : secondar SLCobject
+    pol : polarization list 
+
+    Returns
+    -------
+    mode : dict for slc to be bandpassed
     """
     mode = dict()
 
     for freq, pol_list in pols.items():
-        ref_meta_data = meta_data_bandpass(ref_slc, freq)
-        sec_meta_data = meta_data_bandpass(sec_slc, freq)
+        ref_meta_data = get_meta_data_bandpass(ref_slc, freq)
+        sec_meta_data = get_meta_data_bandpass(sec_slc, freq)
+        
+        ref_wvl = ref_meta_data['wavelength']
+        sec_wvl = sec_meta_data['wavelength']
+        ref_bw = ref_meta_data['rg_bandwidth']
+        sec_bw = sec_meta_data['rg_bandwidth']
 
         # check if two SLCs have same bandwidth and center frequency
-        if (ref_meta_data['wavelength'] != sec_meta_data['wavelength'] ) \
-        or (ref_meta_data['rg_bandwidth'] != sec_meta_data['rg_bandwidth']):
-
-            if ref_meta_data['rg_bandwidth'] > sec_meta_data['rg_bandwidth']:
+        if (ref_wvl != sec_wvl) or (ref_bw != sec_bw):
+            if ref_bw > sec_bw:
                 mode[freq] = 'ref'
             else:
                 mode[freq] = 'sec'
     return mode
 
 
-class Splitspectrum:
-    
-    def __init__(self):
-        self.frame = ""
-        self.slc_raster = 0
-        self.fft_size = 0
-        self.width = 0
-        self.low_frequency = 0
-        self.high_frequency = 0
-        self.new_center_frequency = 0
-        self.freq = 'A'
-        self.pol = 'HH'
+class SplitSpectrum:
+    '''
+    Split the range spectrum in InSAR workflow
+    '''
+
+    def __init__(self, rslc_product, freq):
+        self.rslc_product = rslc_product
+        self.freq = freq
+        rdr_grid = self.rslc_product.getRadarGrid(self.freq)
+        self.rg_pxl_spacing = rdr_grid.range_pixel_spacing
+        self.wavelength = rdr_grid.wavelength
+        self.rg_sample_freq = isce3.core.speed_of_light / 2.0 / self.rg_pxl_spacing
+        self.rg_bandwidth = self.rslc_product.getSwathMetadata(
+            self.freq).processed_range_bandwidth
+        self.center_frequency = isce3.core.speed_of_light / self.wavelength
+        self.slant_range = rdr_grid.slant_range
         self.beta = 0.25
         
-        return None
-      
-    def bandpass_spectrum(self):
-        """
-        Band-pass filter
+    def bandpass_spectrum(self, 
+                          slc_raster, 
+                          low_frequency, 
+                          high_frequency,
+                          new_center_frequency,
+                          beta=None, 
+                          fft_size=None):
+        """bandpass range specturm for given center frequency and bandwidth
+        Parameters
+        ----------
+        slc_raster : numpy array of slc 
+        low_frequency : low  frequency for bandpass [Hz]
+        high_frequency : high frequency for bandpass [Hz]
+        new_center_frequency : new center frequency for bandpass [Hz]
+        beta : parameter for the raised cosine filter
+        fft_size : integer fft size 
+        Returns
+        -------
+        filtered_slc : numpy array of bandpassed slc
+        meta : dict for meta data
+        """       
+        error_channel = journal.error('splitspectrum.bandpass_spectrum')
+ 
+        rg_sample_freq = self.rg_sample_freq
+        rg_bandwidth = self.rg_bandwidth
+        center_frequency = self.center_frequency
+        diff_frequency = self.center_frequency - new_center_frequency
+        height, width = slc_raster.shape
+        slc_raster = np.array(slc_raster, dtype='complex')
+        new_bandwidth = high_frequency - low_frequency
         
-        frame : slc oject to contain parameters
-        slc   : numpy array of slc 
-        fL    : low  frequency for bandpass
-        fH    : high frequency for bandpass
-        cfrequency : new center frequency for bandpass
-        freq : [A , B]
-        """
-        rdr_grid         = self.frame.getRadarGrid(self.freq)
-        rg_pxl_spacing   = rdr_grid.range_pixel_spacing
-        wavelength       = rdr_grid.wavelength
-        rg_sample_freq   = isce3.core.speed_of_light / 2.0 / rg_pxl_spacing
-        rg_bandwidth     = self.frame.getSwathMetadata(self.freq).processed_range_bandwidth
-        center_frequency = isce3.core.speed_of_light / wavelength
-        slant_range      = rdr_grid.slant_range
-        
-        fL = self.low_frequency
-        fH = self.high_frequency
-        
-        diff_frequency = center_frequency - self.new_center_frequency
-        height, width = self.slc_raster.shape
-        slc_raster = np.array(self.slc_raster,dtype='complex')
-        
-        if self.fft_size == 0 : fft_size = width
+        if new_bandwidth < 0:
+            err_str = f"Low and high frequencies values are wrong"
+            error_channel.log(err_str)
+            raise ValueError(err_str)
 
-        window_target = self.constructRangeBandpassCosine(
-                        suband_center_frequencies = 0,
-                        frequencyLH = [- rg_bandwidth/2, 
-                                        rg_bandwidth/2],
-                        samplingFrequency = rg_sample_freq, 
-                        fft_size = fft_size,
-                        beta = 0.25)
+        if fft_size is None:
+            fft_size = width
+        if beta is None:
+            beta = self.beta
 
-        bpfilter = self.constructRangeBandpassCosine(
-                        suband_center_frequencies = center_frequency,
-                        frequencyLH = [fL, fH],
-                        samplingFrequency = rg_sample_freq, 
-                        fft_size = fft_size,
-                        beta = 0.25)
-        
-        resampling_factor = rg_bandwidth / int(np.abs(fH-fL))
-        sub_fft_size = int(fft_size/resampling_factor)
-        power_factor = resampling_factor /1.1
+        window_target = self.construct_range_bandpass_cosine(
+            suband_center_frequencies=0,
+            frequencyLH=[-rg_bandwidth/2, 
+                          rg_bandwidth/2],
+            sampling_frequency=rg_sample_freq, 
+            fft_size=fft_size,
+            beta=beta
+        )
+
+        bpfilter = self.construct_range_bandpass_cosine(
+            suband_center_frequencies=center_frequency,
+            frequencyLH=[low_frequency, high_frequency],
+            sampling_frequency=rg_sample_freq, 
+            fft_size=fft_size,
+            beta=beta
+        )
+
+        resampling_scale_factor = rg_bandwidth / int(np.abs(new_bandwidth))
+        sub_fft_size = int(fft_size/resampling_scale_factor)
 
         # remove the windowing effect from the spectrum 
-        spectrum_target = fft(np.array(slc_raster)) / np.tile(window_target,[height, 1])
+        spectrum_target = fft(np.array(slc_raster)) / \
+                              np.tile(window_target, [height, 1])
         # apply new bandpass window to spectrum 
-        slc_bp = ifft(spectrum_target * np.tile(bpfilter,[height, 1])*np.sqrt(power_factor))        
+        slc_bp = ifft(spectrum_target 
+                      * np.tile(bpfilter, [height, 1]) 
+                      * np.sqrt(resampling_scale_factor))      
+
         # demodulate the SLC to be baseband to new center frequency
-        slc_demodulate = self.demodulate_slc(slc_bp, diff_frequency, rg_sample_freq)
+        slc_demodulate = self.demodulate_slc(slc_bp, 
+                                             diff_frequency, 
+                                             rg_sample_freq)
 
         # resample SLC 
-        filtered_slc = resample(slc_demodulate, sub_fft_size ,axis=1)
+        filtered_slc = resample(slc_demodulate, sub_fft_size, axis=1)
 
         meta = dict()
-        meta['centerFrequency'] = self.new_center_frequency
-        meta['Bandwidth'] = fH - fL
-        meta['rangeSpacing'] = rg_pxl_spacing * resampling_factor
-        meta['slantRange'] =  np.linspace(slant_range(0),slant_range(fft_size),\
-                            sub_fft_size,endpoint=False)
+        meta['center_frequency'] = new_center_frequency
+        meta['rg_bandwidth'] = new_bandwidth
+        meta['range_spacing'] = self.rg_pxl_spacing * resampling_scale_factor
+        meta['slant_range'] = np.linspace(self.slant_range(0), self.slant_range(fft_size),\
+                            sub_fft_size, endpoint=False)
 
         return filtered_slc, meta
 
-    def demodulate_slc(self, 
-                       slc_array, diff_frequency, rg_sample_freq):
-        """
-        demodulate slc_array for a given frequecy diff_frequency
-        
-        slc   : numpy array of SLC 
-        diff_frequency : frequency difference between old and new slcs
-        rg_sample_freq : range sampling frequency       
+    def demodulate_slc(self, slc_array, diff_frequency, rg_sample_freq):
+        """ demodulate slc 
+
+        Parameters
+        ----------
+        slc_array : slc numpy array
+        diff_frequency : shift frequency [Hz] 
+        rg_sample_freq : range sampling frequency [Hz]
+        Returns
+        -------
+        slc_de  : demodulated slc
         """
         height, width = np.shape(slc_array)
 
-        rangeTime=np.zeros(width)
+        range_time=np.zeros(width)
         for ii in range(width):
-            rangeTime[ii] = ii / rg_sample_freq
+            range_time[ii] = ii / rg_sample_freq
         
-        slc_de = np.zeros([height,width],dtype=complex)
+        slc_de = np.zeros([height, width], dtype=complex)
         for jj in range(height):
-            slc_de[jj,:] = slc_array[jj,:] * np.exp(-1 * 1j * 2.0 * np.pi * -1 * \
-                            diff_frequency * rangeTime)
+            slc_de[jj, :] = slc_array[jj,:] * np.exp(-1 * 1j * 2.0 * np.pi * -1 
+                                                    * diff_frequency * range_time)
         return slc_de
 
-    def freqSpectrum(self, cfrequency, dt, fft_size):
+    def freq_spectrum(self, cfrequency, dt, fft_size):
         freq = cfrequency + fftfreq(fft_size, dt)
         return freq
 
-    def constructRangeBandpassCosine(self, suband_center_frequencies,
-                                    samplingFrequency,
-                                    fft_size,
-                                    frequencyLH,
-                                    beta):
-       
-        frequency = self.freqSpectrum(
-                    cfrequency = suband_center_frequencies,
-                    dt = 1.0 / samplingFrequency,
-                    fft_size = fft_size) 
+    def construct_range_bandpass_cosine(self, 
+                                        suband_center_frequencies,
+                                        sampling_frequency,
+                                        fft_size,
+                                        frequencyLH,
+                                        beta):
+        '''Turkey window
+        Parameters
+        ----------
+        suband_center_frequencies : center frequency for passband [Hz]
+        sampling_frequency :  sampling frequency [Hz]
+        fft_size : low  frequency for bandpass [Hz]
+        frequencyLH : vector of low and high frequency [Hz]
+        beta : parameter for the raised cosine filter
 
-        freq_size=np.shape(frequency)[0]
-        norm = 1.0;    
+        Returns
+        -------
+        filter_1d : one dimensional cosine bandpass filter in frequency domain
+        '''
+        frequency = self.freq_spectrum(
+                        cfrequency = suband_center_frequencies,
+                        dt = 1.0 / sampling_frequency,
+                        fft_size = fft_size
+                        ) 
+
+        freq_size = np.shape(frequency)[0]
+        norm = 1.0 
         fL, fH = frequencyLH
-        fmid = (fH + fL) * 0.5 
-        subbandwidth = np.abs( fH - fL )
+        freq_mid = 0.5 * (fH+fL)
+        subbandwidth = np.abs(fH-fL)
         df = 0.5 * subbandwidth * beta
 
-        filter_1d=np.zeros([freq_size],dtype='complex')
-        for i in range(0,freq_size):
+        filter_1d = np.zeros([freq_size], dtype='complex')
+        for i in range(0, freq_size):
 
-            #Get the absolute value of shifted frequency
+            # Get the absolute value of shifted frequency
             freq = np.abs(frequency[i])
-            freqabs = np.abs(freq- fmid)
+            freqabs = np.abs(freq - freq_mid)
 
             # Passband
             if (freq <= (fH - df)) and (freq >= (fL + df)):
@@ -181,7 +242,8 @@ class Splitspectrum:
 
             # Transition region
             elif ((freq < (fL + df)) and (freq >= (fL - df))) \
-            or ((freq <= (fH + df)) and (freq > (fH - df))):
-                filter_1d[i] = norm * 0.5 * (1.0 + np.cos(np.pi / (subbandwidth*beta) *
-                            (freqabs - 0.5 * (1.0 - beta) * subbandwidth)))
+                    or ((freq <= (fH + df)) and (freq > (fH - df))):
+                filter_1d[i] = norm * 0.5 * (1.0 
+                             + np.cos(np.pi / (subbandwidth*beta) 
+                             * (freqabs - 0.5 * (1.0 - beta) * subbandwidth)))
         return filter_1d
