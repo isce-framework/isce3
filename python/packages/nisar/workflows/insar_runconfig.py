5b73a14a07a5a61e8dccf83ac06a127b2a87cdf5
Virginia Brancato <virginia.brancato@jpl.nasa.gov>
2021-11-22 11:02:21 -0800

    Including split-spectrum in InSAR workflow

--- python/packages/nisar/workflows/insar_runconfig.py
+++ python/packages/nisar/workflows/insar_runconfig.py
@@ -1,6 +1,8 @@
+from nisar.products.readers import SLC
 from nisar.workflows.geo2rdr_runconfig import Geo2rdrRunConfig
 import journal
 import os
+import h5py
 
 class InsarRunConfig(Geo2rdrRunConfig):
     def __init__(self, args):
@@ -133,6 +135,57 @@ class InsarRunConfig(Geo2rdrRunConfig):
         if algorithm not in self.cfg['processing']['phase_unwrap']:
             self.cfg['processing']['phase_unwrap'][algorithm]={}
 
+        # Create defaults for ionosphere phase correction
+        iono_cfg = self.cfg['processing']['ionosphere_phase_correction']
+        # If ionosphere phase correction is enabled, check defaults
+        if iono_cfg['enabled']:
+            # Extract split-spectrum dictionary
+            split_cfg = iono_cfg['range_split_spectrum']
+            # Extract main range bandwidth from reference SLC
+            ref_slc = SLC(hdf5file=self.cfg['InputFileGroup']['InputFilePath'])
+            rg_main_bandwidth = ref_slc.getSwathMetadata(
+                'A').processed_range_bandwidth
+
+            # Depending on how the user has selected "spectral_diversity" check if
+            # "low_bandwidth" and "high_bandwidth" are assigned. Otherwise, use default
+            if split_cfg['spectral_diversity'] == 'split_main_band':
+                # If "low_bandwidth" or 'high_bandwidth" is not allocated, split the main range bandwidth
+                # into two 1/3 sub-bands.
+                if split_cfg['low_band_bandwidth'] is None:
+                    split_cfg['low_band_bandwidth'] = rg_main_bandwidth / 3.0
+                if split_cfg['high_band_bandwidth'] is None:
+                    split_cfg['high_band_bandwidth'] = rg_main_bandwidth / 3.0
+
+            if split_cfg['spectral_diversity'] == 'main_side_band':
+                # Extract side-band range bandwidth
+                rg_side_bandwidth = ref_slc.getSwathMetadata(
+                    'B').processed_range_bandwidth
+
+                # If "low_bandwidth" and "high_bandwidth" are not assigned, assign main range bandwidth
+                # and side-band bandwidths, respectively. If assigned, check that
+                # "low_bandwidth" and "high_bandwidth" correspond to main and side range bandwidths
+                if split_cfg['low_band_bandwidth'] is None or split_cfg[
+                    'low_band_bandwidth'] != rg_main_bandwidth:
+                    split_cfg['low_band_bandwidth'] = rg_main_bandwidth
+                if split_cfg['high_band_bandwidth'] is None or split_cfg[
+                    'high_band_bandwidth'] != rg_side_bandwidth:
+                    split_cfg['high_band_bandwidth'] = rg_side_bandwidth
+
+                # Check that main and side-band are at the same polarization. If not, throw an error.
+                src_h5 = h5py.File(self.cfg['InputFileGroup']['InputFilePath'],
+                                   'r',
+                                   libver='latest', swmr=True)
+                pol_path = os.path.join(ref_slc.SwathPath, 'frequencyA',
+                                        'listOfPolarizations')
+                pols_freqA = src_h5[pol_path][()]
+                pol_path = os.path.join(ref_slc.SwathPath, 'frequencyB',
+                                        'listOfPolarizations')
+                pols_freqB = src_h5[pol_path][()]
+                src_h5.close()
+                if len(set.intersection(set(pols_freqA), set(pols_freqB))) == 0:
+                    err_str = "No common polarization between frequency A and B rasters"
+                    error_channel.log(err_str)
+                    raise FileNotFoundError(err_str)
 
         if 'interp_method' not in self.cfg['processing']['geocode']:
             self.cfg['processing']['geocode']['interp_method'] = 'BILINEAR'
