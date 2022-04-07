Fix SNAPHU IntegratePhase() to prevent accumulating large numerical error (#967)
* Update IntegratePhase() to resolve numerical precision issues

SNAPHU's IntegratePhase() function determines the unwrapped phase by
accumulating the discrete unwrapped phase gradients along an integration
path that spans the interferogram (tile). It first scans across the
first row, and then down each column.

In practice, this can lead to unacceptably large numerical precision
errors by summing thousands of floating point numbers.

This update modifies the algorithm to instead integrate an integer
number of cycles of 2pi in order to avoid the accumulation of floating
point rounding errors. The residual phase error is significantly
improved and, as a result, the absolute error tolerances used in the
SNAPHU unit tests could be decreased by an order of magnitude.

In addition, the order in which we integrate over the interferogram was
changed from column-by-column to row-by-row, in order to improve memory
locality for row-major data structures like those currently used in
SNAPHU.

* Update SNAPHU tests to use both abs. & rel. error tolerances

tests/python/packages/isce3/unwrap/snaphu.py
@@ -261,7 +261,7 @@ class TestSnaphu:
             mphase = phase[mask]
             munw = unw_raster.data[mask]
             offset = mphase[0] - munw[0]
-            assert np.allclose(munw + offset, mphase, rtol=0.0, atol=1e-5)
+            assert np.allclose(mphase - offset, munw, rtol=1e-6, atol=1e-6)
 
         # Check the set of unique labels (masked-out pixels are labeled 0).
         unique_cc_labels = set(np.unique(ccl_raster.data))
@@ -371,7 +371,7 @@ class TestSnaphu:
         mphase = phase[mask]
         munw = unw_raster.data[mask]
         offset = mphase[0] - munw[0]
-        good_pixels = np.isclose(munw + offset, mphase, rtol=0.0, atol=1e-4)
+        good_pixels = np.isclose(mphase - offset, munw, rtol=1e-6, atol=1e-6)
         assert frac_nonzero(~good_pixels) < 1e-3
 
     def test_tile_mode(self):
@@ -437,4 +437,4 @@ class TestSnaphu:
             mphase = phase[mask]
             munw = unw_raster.data[mask]
             offset = mphase[0] - munw[0]
-            assert np.allclose(munw + offset, mphase, rtol=0.0, atol=1e-5)
+            assert np.allclose(mphase - offset, munw, rtol=1e-6, atol=1e-6)