443324458a09a5f04b7cb39a73dcd640a5b72507
Virginia Brancato <virginia.brancato@jpl.nasa.gov>
2022-07-18 09:00:17 -0700

    Geocode ROFF product (GOFF generation) (#1055)
    * Expose offsets processing options
    
    * Generate standalone ROFF product
    
    * Include offsets product computation in InSAR workflow
    
    * Revert "Expose offsets processing options"
    
    This reverts commit e3ce076edde8eaf0cba2c3a19b291380224c7ea4.
    
    * Revert "Generate standalone ROFF product"
    
    This reverts commit 96b46d8e00362a9ed00ed5541adbc704a49a144d.
    
    * Revert "Include offsets product computation in InSAR workflow"
    
    This reverts commit ba7986d5f5f2c0805fe66b00cd720d2543c06fd9.
    
    * Add geocode of ROFF product
    
    * Add ROFF geocoding in InSAR workflow
    
    * Add default parameters for offset product
    
    * Modify geocode_insar unit test
    
    * Correcting allocation of GUNW and GOFF datasets
    
    * Change name of input product and improve line spacing
    
    * refactor allocation of default datasets in insar/geocode runconfigs
    
    * refactor parameter selection for offsets radar grid
    
    * Add default offset layer
    
    * Compact notation for source and destination path to RUNW and ROFF prods

--- python/packages/nisar/workflows/insar_runconfig.py
+++ python/packages/nisar/workflows/insar_runconfig.py
@@ -355,18 +355,27 @@ class InsarRunConfig(Geo2rdrRunConfig):
             self.cfg['processing']['geocode']['interp_method'] = 'BILINEAR'
 
         # create empty dict if geocode_datasets not in geocode
-        if 'datasets' not in self.cfg['processing']['geocode']:
-            self.cfg['processing']['geocode']['datasets'] = {}
+        for datasets in ['gunw_datasets', 'goff_datasets']:
+            if datasets not in self.cfg['processing']['geocode']:
+                self.cfg['processing']['geocode'][datasets] = {}
 
-        # default to True for datasets not found
-        gunw_datasets = ["connected_components", "coherence_magnitude",
-                         "unwrapped_phase", "along_track_offset",
-                         "slant_range_offset", 'layover_shadow_mask']
-
-        for gunw_dataset in gunw_datasets:
-            if gunw_dataset not in self.cfg['processing']['geocode']['datasets']:
-                self.cfg['processing']['geocode']['datasets'][
-                    gunw_dataset] = True
+        # Initialize GUNW and GOFF names
+        gunw_datasets = ['connected_components', 'coherence_magnitude',
+                         'unwrapped_phase', 'along_track_offset',
+                         'slant_range_offset', 'layover_shadow_mask']
+        goff_datasets = ['along_track_offset', 'snr',
+                         'along_track_offset_variance',
+                         'correlation_surface_peak', 'cross_offset_variance',
+                         'slant_range_offset', 'slant_range_offset_variance']
+        # insert both geocode datasets in dict keyed on datasets name
+        geocode_datasets = {'gunw_datasets': gunw_datasets,
+                            'goff_datasets': goff_datasets}
+        for dataset_group in geocode_datasets:
+            for dataset in geocode_datasets[dataset_group]:
+                if dataset not in self.cfg['processing']['geocode'][
+                    dataset_group]:
+                    self.cfg['processing']['geocode'][dataset_group][
+                        dataset] = True
 
         # Check if layover shadow output enabled
         if not self.cfg['processing']['rdr2geo']['write_layover_shadow']:
@@ -377,21 +386,3 @@ class InsarRunConfig(Geo2rdrRunConfig):
 
             # Set write flag True
             self.cfg['processing']['rdr2geo']['write_layover_shadow'] = True
-
-        # To geocode the offsets we need the offset field shape and
-        # the start pixel in range and azimuth. Note, margin and gross_offsets
-        # are allocated as defaults in share/nisar/defaults/insar.yaml
-        geocode_azimuth_offset = self.cfg['processing'][
-            'geocode']['datasets']['along_track_offset']
-        geocode_range_offset = self.cfg['processing'][
-            'geocode']['datasets']['slant_range_offset']
-        if geocode_azimuth_offset or geocode_range_offset:
-            offset_cfg = self.cfg['processing']['dense_offsets']
-            margin = max(offset_cfg['margin'], offset_cfg['gross_offset_range'],
-                         offset_cfg['gross_offset_azimuth'])
-            if offset_cfg['start_pixel_range'] is None:
-                offset_cfg['start_pixel_range'] = margin + offset_cfg[
-                    'half_search_range']
-            if offset_cfg['start_pixel_azimuth'] is None:
-                offset_cfg['start_pixel_azimuth'] = margin + offset_cfg[
-                    'half_search_azimuth']
