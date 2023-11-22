2e8c686861b3c878ebe7caae3ad87555ace4cb4c
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-11-22 13:59:49 -0800

    initial commit

--- python/packages/isce3/atmosphere/tec_product.py
+++ python/packages/isce3/atmosphere/tec_product.py
@@ -10,7 +10,11 @@ import journal
 
 import isce3
 from isce3.geometry import compute_incidence_angle
+from scipy.interpolate import RectBivariateSpline
 
+# constants used compute ionospheric range delay
+K = 40.31 # its a constant in m3/s2
+TECU = 1e16 # its a constant to convert the TEC product to electrons / m2
 
 def _compute_ionospheric_range_delay(utc_time: list, tec_json: dict, nr_fr: str,
                                      nr_fr_rg: float, center_freq: float,
@@ -56,17 +60,7 @@ def _compute_ionospheric_range_delay(utc_time: list, tec_json: dict, nr_fr: str,
         TEC delta range
     '''
     # compute sub orbital TEC from total and top TEC in JSON
-    tot_tec = np.array(tec_json[f'totTec{nr_fr}'])
-    top_tec = np.array(tec_json[f'topTec{nr_fr}'])
-    sub_orbital_tec = tot_tec - top_tec
-    sub_orbital_tec = np.array([tec
-                                for tec, m in zip(sub_orbital_tec,
-                                                  tec_time_mask)
-                                if m])
-
-    # constants used compute ionospheric range delay
-    K = 40.31 # its a constant in m3/s2
-    TECU = 1e16 # its a constant to convert the TEC product to electrons / m2
+    sub_orbital_tec = _get_suborbital_tec(tec_json, nr_fr, tec_time_mask)
 
     incidence = [compute_incidence_angle(t, nr_fr_rg, orbit, doppler_lut,
                                          radar_grid, dem_interp, ellipsoid)
@@ -77,7 +71,41 @@ def _compute_ionospheric_range_delay(utc_time: list, tec_json: dict, nr_fr: str,
     return delta_r
 
 
-def tec_lut2d_from_json(json_path: str, center_freq: float,
+def _get_suborbital_tec(tec_json: dict,
+                        nr_fr: str,
+                        tec_time_mask: list):
+    '''
+    Get the suborbital TEC
+
+    Parameters
+    ----------
+    tec_json: dict
+        TEC JSON as a dict. Keys are TEC parameters and values are the values
+        of said parameter.
+    nr_fr: ['Nr', 'Fr']
+        String values used to access Near or Far TEC parameters in TEC JSON
+        dict
+    tec_time_mask: list
+        List of bools to extract valid total and top TEC values
+
+    Returns
+    -------
+    sub_orbital_tec: np.ndarray
+        Suborbital TEC
+    '''
+    # compute sub orbital TEC from total and top TEC in JSON
+    tot_tec = np.array(tec_json[f'totTec{nr_fr}'])
+    top_tec = np.array(tec_json[f'topTec{nr_fr}'])
+    sub_orbital_tec = tot_tec - top_tec
+    sub_orbital_tec = np.array([tec
+                                for tec, m in zip(sub_orbital_tec,
+                                                  tec_time_mask)
+                                if m])
+
+    return sub_orbital_tec
+
+
+def tec_lut2d_from_json_srg(json_path: str, center_freq: float,
                         orbit: isce3.core.Orbit,
                         radar_grid: isce3.product.RadarGridParameters,
                         doppler_lut: isce3.core.LUT2d, dem_path: str,
@@ -155,3 +183,94 @@ def tec_lut2d_from_json(json_path: str, center_freq: float,
                          for nr_fr, rg in zip(['Nr', 'Fr'], rg_vec)]).T
 
     return isce3.core.LUT2d(rg_vec, utc_time, delta_r)
+
+
+def tec_lut2d_from_json_az(json_path: str, center_freq: float,
+                        orbit: isce3.core.Orbit,
+                        radar_grid: isce3.product.RadarGridParameters,
+                        doppler_lut: isce3.core.LUT2d,
+                        margin: float=40.0) -> isce3.core.LUT2d:
+    '''
+    Create a TEC LUT2d for azimuth time correction from a JSON source
+
+    Parameters
+    ----------
+    json_path: str
+        Path to JSON file containing TEC data
+    center_freq: float
+        Processed center frequency of swath (Hz)
+    orbit: isce3.core.Orbit
+        Orbit for associated SLC
+    radar_grid: isce3.product.RadarGridParameters
+        Radar grid for associated SLC
+    doppler_lut: isce3.core.LUT2d
+        Doppler centroid of SLC
+    dem_path: str
+        Digital elevation model, m above ellipsoid. Defaults to h=0.
+    margin: float
+        Margin (seconds) to pad to sensing start and stop times when extracting
+        TEC data. Default 40 seconds.
+
+    Returns
+    -------
+    isce3.core.LUT2d
+        TEC in a LUT2d as function of az time and slant range
+    '''
+    error_channel = journal.error('tec_product.tec_lut2d_from_json')
+
+    if not os.path.isfile(json_path):
+        err_str = f'TEC JSON path not found: {json_path}'
+        error_channel.log(err_str)
+
+    # Load TEC from NISAR TEC JSON file
+    with open(json_path, 'r') as fid:
+        tec_json = json.load(fid)
+
+    # Save UTC time as total seconds since reference epoch of radar grid
+    ref_epoch = datetime.fromisoformat(radar_grid.ref_epoch.isoformat()[:-3])
+    utc_time = [(datetime.fromisoformat(t) - ref_epoch).total_seconds()
+                for t in tec_json['utc']]
+
+    # Filter utc_time to only save times near radar_grid.
+    # Pad data before and after to ensure enough TEC data is collected.
+    # Pad with length of one burst should suffice.
+    t_lower_bound = radar_grid.sensing_start - margin
+    t_upper_bound = radar_grid.sensing_stop + margin
+    time_mask = [t >= t_lower_bound and t <= t_upper_bound for t in utc_time]
+    utc_time = np.array([t for t, m in zip(utc_time, time_mask) if m])
+
+
+    # Using zero DEM in current implementation to account for TEC file bounds
+    # being larget than that of the scene DEM
+
+    # Compute near and far delta range for near and far TEC
+    # Use radar grid start/end range for near/far range
+    # Transpose stacked output to get shape to be consistent with coordinates
+    rg_vec = [radar_grid.starting_range, radar_grid.end_range]
+    tec_suborbital = np.vstack([_get_suborbital_tec(tec_json,
+                                                    nr_fr, time_mask)
+                                for nr_fr in ['Nr', 'Fr']]).T
+
+    # set up up the LUT grids for az. iono. delay
+    shape_lut = (int((radar_grid.shape[0] + 0.5) // 30), int((radar_grid.shape[1] + 0.5) // 30))
+    utc_vec_lut = np.linspace(radar_grid.sensing_start, radar_grid.sensing_stop, shape_lut[0])
+    rg_vec_lut = np.linspace(radar_grid.starting_range, radar_grid.end_range, shape_lut[1])
+
+    speed_vec_lut = np.array([np.linalg.norm(orbit.interpolate(t)[1]) for t in utc_vec_lut])
+
+    az_fm_rate = np.outer(-2 * speed_vec_lut**2 * (center_freq / isce3.core.speed_of_light),
+                          1/rg_vec_lut)
+
+    # Calculate tec gradient
+    spacing_utc_lut = (utc_vec_lut[-1] - utc_vec_lut[0]) / (utc_vec_lut.size - 1)
+    utc_vec_shifted = utc_vec_lut - spacing_utc_lut / 2
+    utc_vec_staggered = np.append(utc_vec_shifted, utc_vec_shifted[-1] + spacing_utc_lut)
+
+    f_tec = RectBivariateSpline(utc_time, rg_vec, tec_suborbital, kx=1, ky=1)
+    tec_i = f_tec(utc_vec_staggered, rg_vec_lut)
+    d_tec_over_d_t = np.diff(tec_i, axis=0) / spacing_utc_lut
+    alpha = 40.31
+
+    t_az_delay = -2 * alpha / (isce3.core.speed_of_light * az_fm_rate * center_freq) * d_tec_over_d_t * TECU
+
+    return isce3.core.LUT2d(rg_vec_lut, utc_vec_lut, t_az_delay)
--- python/packages/nisar/workflows/geocode_corrections.py
+++ python/packages/nisar/workflows/geocode_corrections.py
@@ -2,25 +2,52 @@
 Compute azimuth and slant range geocoding corrections as LUT2d
 '''
 import isce3
-from isce3.atmosphere.tec_product import tec_lut2d_from_json
-
+from isce3.atmosphere.tec_product import tec_lut2d_from_json_srg, tec_lut2d_from_json_az
 from nisar.products.readers import SLC
 
 
-def _get_accumulated_azimuth_corrections():
+def _get_accumulated_azimuth_corrections(cfg, slc, frequency, orbit):
     '''
-    Compute, accumulate, and return azimuth time corrections as LUT2d. Default
+    Compute, accumulate, and azimuth geolocation corrections as LUT2d. Default
     to default LUT2d if provided parameters do not require corrections to be
     computed.
 
-    Yields:
+    Currently on TEC corrections available. Others will be added as they
+    become available.
+
+    Parameters
+    ----------
+    cfg: dict
+        Dict containing the runconfiguration parameters
+    slc: nisar.products.readers.SLC
+        NISAR single look complex (SLC) object containing swath and radar grid
+        parameters
+    frequency: ['A', 'B']
+        Str identifcation for NISAR SLC frequencies
+    orbit: isce3.core.Orbit
+        Object containing orbit associated with SLC
+
+    Returns
     -------
-    _: isce3.core.LUT2d
-        Azimuth correction for geocoding. Currently only no corrections are
-        computed and a default isce3.core.LUT2d is be passed back.
+    tec_correction: isce3.core.LUT2d
+        Azimith correction for geocoding. Currently only TEC corrections
+        are considered. If no TEC JSON file is provided in the cfg parameter,
+        a default isce3.core.LUT2d will be passed back.
     '''
-    # empty until actual correction computations are ready for integration
-    return isce3.core.LUT2d()
+    # Compute TEC slant range correction if TEC file is provided
+    tec_file = cfg["dynamic_ancillary_file_group"]['tec_file']
+    if tec_file is not None:
+        # Get SLC object for parameters inside necessary for TEC computations
+        input_hdf5 = cfg['input_file_group']['input_file_path']
+        slc = SLC(hdf5file=input_hdf5)
+        center_freq = slc.getSwathMetadata(frequency).processed_center_frequency
+        doppler = isce3.core.LUT2d()
+        radar_grid = slc.getRadarGrid(frequency)
+
+        tec_correction = tec_lut2d_from_json_az(tec_file, center_freq, orbit,
+                                                radar_grid, doppler)
+
+    return tec_correction
 
 
 def _get_accumulated_srange_corrections(cfg, slc, frequency, orbit):
@@ -65,8 +92,8 @@ def _get_accumulated_srange_corrections(cfg, slc, frequency, orbit):
         # DEM file for DEM interpolator and ESPF for ellipsoid
         dem_file = cfg['dynamic_ancillary_file_group']['dem_file']
 
-        tec_correction = tec_lut2d_from_json(tec_file, center_freq, orbit,
-                                             radar_grid, doppler, dem_file)
+        tec_correction = tec_lut2d_from_json_srg(tec_file, center_freq, orbit,
+                                                 radar_grid, doppler, dem_file)
 
     return tec_correction
 
@@ -99,7 +126,8 @@ def get_az_srg_corrections(cfg, slc, frequency, orbit):
         are considered. If no TEC JSON file is provided in the cfg parameter,
         a default isce3.core.LUT2d will be passed back.
     '''
-    az_corrections = _get_accumulated_azimuth_corrections()
+    az_corrections = _get_accumulated_azimuth_corrections(cfg, slc,
+                                                          frequency, orbit)
     srange_corrections = _get_accumulated_srange_corrections(cfg, slc,
                                                              frequency, orbit)
 
