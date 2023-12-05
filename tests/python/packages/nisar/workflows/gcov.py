620e2676947758efb01c692858f7f0e58139db8b
Gustavo H Shiroma <Gustavo.H.Shiroma@jpl.nasa.gov>
2023-12-04 18:22:00 -0800

    Quick PR to update GCOV workflow unit test delete previous product (#1593)
    * rename BaseWriter to BaseWriterSingleInput
    
    * delete previous GCOV product before creating a new one
    
    * Revert changes that are not part of this PR
    
    * Update tests/python/packages/nisar/workflows/gcov.py
    
    Co-authored-by: Hirad Ghaemi <Hirad.Ghaemi@jpl.nasa.gov>
    
    ---------
    
    Co-authored-by: Hirad Ghaemi <Hirad.Ghaemi@jpl.nasa.gov>

--- tests/python/packages/nisar/workflows/gcov.py
+++ tests/python/packages/nisar/workflows/gcov.py
@@ -9,9 +9,10 @@ from nisar.products.writers import GcovWriter
 
 import iscetest
 
-geocode_modes = {'interp':isce3.geocode.GeocodeOutputMode.INTERP,
-        'area':isce3.geocode.GeocodeOutputMode.AREA_PROJECTION}
+geocode_modes = {'interp': isce3.geocode.GeocodeOutputMode.INTERP,
+                 'area': isce3.geocode.GeocodeOutputMode.AREA_PROJECTION}
 input_axis = ['x', 'y']
+
 
 def test_run():
     '''
@@ -19,10 +20,12 @@ def test_run():
     '''
     test_yaml = os.path.join(iscetest.data, 'geocode/test_gcov.yaml')
 
-    # load text then substitude test directory paths since data dir is read only
+    # load text then substitude test directory paths since data dir is read
+    # only
     with open(test_yaml) as fh_test_yaml:
         test_yaml = ''.join(
-                [line.replace('@ISCETEST@', iscetest.data) for line in fh_test_yaml])
+            [line.replace('@ISCETEST@', iscetest.data)
+             for line in fh_test_yaml])
 
     # create CLI input namespace with yaml text instead of file path
     args = argparse.Namespace(run_config_path=test_yaml, log_file=False)
@@ -35,7 +38,12 @@ def test_run():
     for axis in input_axis:
         #  iterate thru geocode modes
         for key, value in geocode_modes.items():
-            runconfig.cfg['product_path_group']['sas_output_file'] = f'{axis}_{key}.h5'
+            sas_output_file = f'{axis}_{key}.h5'
+            runconfig.cfg['product_path_group']['sas_output_file'] = \
+                sas_output_file
+
+            if os.path.isfile(sas_output_file):
+                os.remove(sas_output_file)
 
             # geocode test raster
             gcov.run(runconfig.cfg)
