e3d32ba6cb9dfaa12e3c4c74f835f3290f95965e
thudson <tyler.g.hudson@jpl.nasa.gov>
2024-02-27 19:58:17 +0000

    call addbindings temlates in image.cpp pybind

--- python/extensions/pybind_isce3/image/image.cpp
+++ python/extensions/pybind_isce3/image/image.cpp
@@ -11,7 +11,8 @@ void addsubmodule_image(py::module & m)
     py::module m_image_v2 = m_image.def_submodule("v2");
 
     // Add the resample v2 functionality to the v2 module.
-    addbindings_resamp(m_image_v2);
+    addbindings_resamp<isce3::core::LUT2d<double>>(m_image_v2);
+    addbindings_resamp<isce3::core::Poly2d>(m_image_v2);
 
     // forward declare bound classes for v1
     py::class_<isce3::image::ResampSlc> pyResampSlc(m_image, "ResampSlc");
