e6de5061983a9ef3f46426fcd4198a63e5300ac2
Gustavo H Shiroma <Gustavo.H.Shiroma@jpl.nasa.gov>
2025-01-23 10:55:08 -0800

    Add "Custom" (OD) processing type (#2113)

--- python/packages/nisar/products/writers/BaseWriterSingleInput.py
+++ python/packages/nisar/products/writers/BaseWriterSingleInput.py
@@ -667,11 +667,13 @@ class BaseWriterSingleInput():
             self.cfg['primary_executable']['processing_type']
 
         if processing_type_runconfig == 'PR':
-            processing_type = np.bytes_('NOMINAL')
+            processing_type = np.bytes_('Nominal')
         elif processing_type_runconfig == 'UR':
-            processing_type = np.bytes_('URGENT')
+            processing_type = np.bytes_('Urgent')
+        elif processing_type_runconfig == 'OD':
+            processing_type = np.bytes_('Custom')
         else:
-            processing_type = np.bytes_('UNDEFINED')
+            processing_type = np.bytes_('Undefined')
         self.set_value(
             'identification/processingType',
             processing_type,
