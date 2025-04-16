873d8f1db74f139828ae3fce136147b0b90261a4
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2025-04-16 23:05:33 +0000

    Override `OGREnvelope::merge`

--- cxx/isce3/geometry/Shapes.h
+++ cxx/isce3/geometry/Shapes.h
@@ -18,9 +18,8 @@ namespace isce3 { namespace geometry {
         See: https://gdal.org/doxygen/ogr__core_8h_source.html */
     class BoundingBox : public OGREnvelope {
         public:
-        /*Same as OGREnvelope::Merge but account for dateline crossing case
-        in geographic coordinates.*/
-        void Merge2(const BoundingBox& other, int epsg=-1) {
+        //Overload OGREnvelope::Merge by adding parameter for EPSG in geographic coordinates.
+        void Merge(const BoundingBox& other, int epsg=-1) {
             const double one_cycle = 360.0;
 
             double minx_global = (MinX < other.MinX) ? MinX : other.MinX;
