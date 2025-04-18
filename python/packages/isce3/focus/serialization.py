c23116ee9bfa18c5336e5a7c149a52878566706c
bhawkins <bhawkins@github.com>
2025-04-18 11:08:12 -0700

    add parsers for FBP junk

--- python/packages/isce3/focus/serialization.py
+++ python/packages/isce3/focus/serialization.py
@@ -17,6 +17,22 @@ def save_linspace_to_h5(x: Linspace, group: h5py.Group):
         overwrite(group, key, val)
 
 
+def load_linspace_from_h5(group: h5py.Group) -> Linspace:
+    args = [group[key][()] for key in ("first", "spacing", "size")]
+    return Linspace(*args)
+
+
+def save_lookside_to_h5(side: LookSide, group: h5py.Group):
+    side_str = str(side).split(".")[1]
+    overwrite(group, "look_side", np.bytes_(side_str))
+
+
+def load_lookside_from_h5(group: h5py.Group) -> LookSide:
+    side_str_lower = group["look_side"][()].decode("utf-8").lower()
+    valid_sides = {"left": LookSide.Left, "right": LookSide.Right}
+    return valid_sides[side_str_lower]
+
+
 def save_polar_grid_to_h5(grid: PolarGrid, group: h5py.Group):
     for key in ("aztime_start", "aztime_end", "origin", "axis"):
         val = getattr(grid, key)
@@ -25,11 +41,25 @@ def save_polar_grid_to_h5(grid: PolarGrid, group: h5py.Group):
         g = group.require_group(key)
         val = getattr(grid, key)
         save_linspace_to_h5(val, g)
-    side_str = str(grid.look_side).split(".")[1]
-    overwrite(group, "look_side", np.bytes_(side_str))
+    save_lookside_to_h5(grid.look_side, group)
+
+
+def load_polar_grid_from_h5(group: h5py.Group) -> PolarGrid:
+    keys = ("aztime_start", "aztime_end", "origin", "axis")
+    args = [group[key][()] for key in keys]
+    args.append(load_linspace_from_h5(group["range"]))
+    args.append(load_linspace_from_h5(group["sin_squint"]))
+    args.append(load_lookside_from_h5(group))
+    return PolarGrid(*args)
 
 
 def save_polar_image_to_h5(z: np.ndarray, grid: PolarGrid, group: h5py.Group):
     overwrite(group, "image", z)
     g = group.require_group("polar_grid")
     save_polar_grid_to_h5(grid, g)
+
+
+def load_polar_image_from_h5(group: h5py.Group) -> tuple[np.ndarray, PolarGrid]:
+    z = group["image"][:]
+    grid = load_polar_grid_from_h5(group["polar_grid"])
+    return (z, grid)
