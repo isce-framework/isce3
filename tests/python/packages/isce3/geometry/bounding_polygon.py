174c98b0232ca679bc78a2794e91ca6a714f620b
Geoffrey M Gunter <geoffrey.m.gunter@jpl.nasa.gov>
2025-07-10 06:56:45 +0000

    Fix edge cases
    * Ensure longitude coordinates are wrapped to the interval [-180, 180],
      except in the case of an antimeridian crossing.
    * Unwrap longitude values so that the polygon is still convex with CCW
      winding order in case of an antimeridian crossing.
    * Only allow latitude values in the interval [-90, 90]. Geo grids with
      latitude values extending outside this range may be self-intersecting.
    * Ensure that the polygon has CCW winding order in geodetic coordinates,
      even if the geo grid was not north-up/west-left and/or the projection
      did not preserve north-south and east-west relative ordering.
    * Add a test case with an antimeridian crossing.
    * Add a test case where the geo grid has north-down orientation.
    * Add a test case that ensures the function raises an exception if the
      latitude coordinates of the geo grid extend outside the interval
      [-90, 90].

--- tests/python/packages/isce3/geometry/bounding_polygon.py
+++ tests/python/packages/isce3/geometry/bounding_polygon.py
@@ -11,18 +11,17 @@ import shapely
 import isce3
 import iscetest
 from isce3.geometry import DEMInterpolator
+from isce3.geometry.bounding_polygon import wrap_to_interval
 from isce3.io import Raster
 from isce3.product import GeoGridParameters
 
 
-def south_pole_dem() -> Raster:
-    dem_raster_path = os.path.join(iscetest.data, "dem_south_pole.tif")
-    return Raster(dem_raster_path)
-
-
-def winnipeg_dem() -> Raster:
-    dem_raster_path = os.path.join(iscetest.data, "winnipeg_dem.tif")
-    return Raster(dem_raster_path)
+def test_wrap_to_interval():
+    assert np.isclose(wrap_to_interval(-181.0, lower=-180.0, upper=180.0), 179.0)
+    assert np.isclose(wrap_to_interval(2 * np.pi, lower=-np.pi, upper=np.pi), 0.0)
+    assert np.isclose(wrap_to_interval(np.pi / 2, lower=-np.pi, upper=np.pi), np.pi / 2)
+    assert np.isclose(wrap_to_interval(0.0, lower=1.0, upper=10.0), 9.0)
+    assert np.isclose(wrap_to_interval(11.0, lower=1.0, upper=10.0), 2.0)
 
 
 def get_raster_grid(raster: isce3.io.Raster) -> GeoGridParameters:
@@ -48,6 +47,55 @@ def get_raster_grid(raster: isce3.io.Raster) -> GeoGridParameters:
         length=raster.length,
         epsg=raster.get_epsg(),
     )
+
+
+def winnipeg() -> tuple[GeoGridParameters, DEMInterpolator]:
+    dem_raster_path = os.path.join(iscetest.data, "winnipeg_dem.tif")
+    dem_raster = Raster(dem_raster_path)
+    geo_grid = get_raster_grid(dem_raster)
+    dem = DEMInterpolator(dem_raster)
+    return geo_grid, dem
+
+
+def south_pole() -> tuple[GeoGridParameters, DEMInterpolator]:
+    dem_raster_path = os.path.join(iscetest.data, "dem_south_pole.tif")
+    dem_raster = Raster(dem_raster_path)
+    geo_grid = get_raster_grid(dem_raster)
+    dem = DEMInterpolator(dem_raster)
+    return geo_grid, dem
+
+
+def antimeridian() -> tuple[GeoGridParameters, DEMInterpolator]:
+    # A geodetic coordinate grid that spans the antimeridian and the equator.
+    geo_grid = GeoGridParameters(
+        start_x=179.0,
+        start_y=1.0,
+        spacing_x=0.01,
+        spacing_y=-0.01,
+        width=201,
+        length=201,
+        epsg=4326,
+    )
+    dem = DEMInterpolator()
+    return geo_grid, dem
+
+
+def north_down() -> tuple[GeoGridParameters, DEMInterpolator]:
+    # A UTM coordinate grid with the Y axis flipped relative to the usual
+    # north-up/west-left orientation. A counterclockwise ring in pixel coordinates on
+    # the grid will be clockwise in map coordinates.
+    geo_grid = GeoGridParameters(
+        start_x=0.0,
+        start_y=0.0,
+        spacing_x=10.0,
+        spacing_y=-10.0,
+        width=1001,
+        length=1001,
+        epsg=32611,
+    )
+    geo_grid.spacing_y = 10.0  # Eww, gross!
+    dem = DEMInterpolator()
+    return geo_grid, dem
 
 
 def get_xy_coords(geo_grid: GeoGridParameters) -> tuple[np.ndarray, np.ndarray]:
@@ -111,11 +159,10 @@ def iter_geo_grid_llh_points(
 
 
 class TestMakeGeoGridBoundingPolygon:
-    @pytest.mark.parametrize("dem_raster", [south_pole_dem(), winnipeg_dem()])
-    def test_contains_geo_grid(self, dem_raster: Raster):
-        geo_grid = get_raster_grid(dem_raster)
-        dem = DEMInterpolator(dem_raster)
-
+    @pytest.mark.parametrize(
+        "geo_grid,dem", [winnipeg(), south_pole(), antimeridian(), north_down()]
+    )
+    def test_contains_geo_grid(self, geo_grid: GeoGridParameters, dem: DEMInterpolator):
         ogr_polygon = isce3.geometry.make_geo_grid_bounding_polygon(
             geo_grid,
             dem,
@@ -128,21 +175,39 @@ class TestMakeGeoGridBoundingPolygon:
 
         assert all(polygon.contains(llh) for llh in llh_points)
 
-    @pytest.mark.parametrize("dem_raster", [south_pole_dem(), winnipeg_dem()])
-    def test_counter_clockwise(self, dem_raster: Raster):
-        geo_grid = get_raster_grid(dem_raster)
-        dem = DEMInterpolator(dem_raster)
-
+    @pytest.mark.parametrize(
+        "geo_grid,dem", [winnipeg(), south_pole(), antimeridian(), north_down()]
+    )
+    def test_counter_clockwise(self, geo_grid: GeoGridParameters, dem: DEMInterpolator):
         ogr_polygon = isce3.geometry.make_geo_grid_bounding_polygon(geo_grid, dem)
         polygon = shapely.wkt.loads(ogr_polygon.ExportToWkt())
-
         assert shapely.is_ccw(polygon.boundary)
+
+    def test_latitude_overflow(self):
+        # A geodetic grid with latitude coordinates extending past -90 degrees. The
+        # bounding polygon of such a grid is self-intersecting (see
+        # https://github.com/isce-framework/isce3/pull/35#discussion_r2178276599).
+        geo_grid = isce3.product.GeoGridParameters(
+            start_x=-118.0,
+            start_y=-88.0,
+            spacing_x=1.0,
+            spacing_y=-1.0,
+            width=10,
+            length=5,
+            epsg=4326,
+        )
+        dem = DEMInterpolator()
+
+        match = (
+            "^geo_grid latitude coordinates must be within the interval"
+            r" \[-pi/2, pi/2\]"
+        )
+        with pytest.raises(ValueError, match=match):
+            isce3.geometry.make_geo_grid_bounding_polygon(geo_grid, dem)
 
     @pytest.mark.parametrize("pts_per_edge", [2, 11])
     def test_num_points(self, pts_per_edge: int):
-        dem_raster = south_pole_dem()
-        geo_grid = get_raster_grid(dem_raster)
-        dem = DEMInterpolator(dem_raster)
+        geo_grid, dem = winnipeg()
 
         ogr_polygon = isce3.geometry.make_geo_grid_bounding_polygon(
             geo_grid,
@@ -155,10 +220,7 @@ class TestMakeGeoGridBoundingPolygon:
         assert n == (4 * pts_per_edge - 3)
 
     def test_bad_pts_per_edge(self):
-        dem_raster = south_pole_dem()
-        geo_grid = get_raster_grid(dem_raster)
-        dem = DEMInterpolator(dem_raster)
-
+        geo_grid, dem = winnipeg()
         with pytest.raises(ValueError, match="^pts_per_edge = 1, must be >= 2$"):
             isce3.geometry.make_geo_grid_bounding_polygon(
                 geo_grid,
