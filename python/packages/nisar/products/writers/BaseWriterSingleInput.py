044d37fdd064829347b032ca8a47148bbfa48f90
Gustavo H. X. Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2025-07-23 16:35:03 -0700

    remove processType "Undefined"

--- python/packages/nisar/products/writers/BaseWriterSingleInput.py
+++ python/packages/nisar/products/writers/BaseWriterSingleInput.py
@@ -665,12 +665,13 @@ class BaseWriterSingleInput():
         elif processing_type_runconfig == 'UR':
             processing_type = np.bytes_('Urgent')
         else:
-            warning_channel = journal.warning(
-                'BaseWriterSingleInput.populate_identification_common()')
-            warning_channel.log(
-                'The processing type in the runconfig is set to'
-                f' "{processing_type_runconfig}", which is not a valid value'
-                ' for the output product metadata. Defaulting to "Custom"')
+            if processing_type_runconfig == 'OD':
+                warning_channel = journal.warning(
+                    'BaseWriterSingleInput.populate_identification_common()')
+                warning_channel.log(
+                    'The processing type in the runconfig is set to'
+                    f' "{processing_type_runconfig}", which is not a valid value'
+                    ' for the output product metadata. Defaulting to "Custom"')
             processing_type = np.bytes_('Custom')
 
         self.set_value(
