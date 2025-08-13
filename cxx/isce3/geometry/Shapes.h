7742f4881d293183493d5867660a00e9ee5ffda5
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2025-08-13 03:14:17 +0000

    remove stdout

--- cxx/isce3/geometry/Shapes.h
+++ cxx/isce3/geometry/Shapes.h
@@ -44,20 +44,17 @@ namespace isce3 { namespace geometry {
             // Compute unit vector from longitude in degrees
             auto deg_to_unitvec = [pi](double deg) {
                 double rad = deg * pi / 180.0;
-                std::cout << "deg_to_unitvec. deg=" << deg << ", vec=(" << std::cos(rad) << "," << std::sin(rad) << ")" << std::endl;
                 return std::pair{std::cos(rad), std::sin(rad)};
             };
 
             // Compute dot product of two 2D vectors
             auto dot = [](const auto &a, const auto &b) {
-                std::cout << "dot. a=(" << a.first << "," << a.second << "), b=(" << b.first << "," << b.second << "), result=" << (a.first * b.first + a.second * b.second) << std::endl;
                 return a.first * b.first + a.second * b.second;
 
             };
 
             // Compute cross product (z-component only in 2D)
             auto cross = [](const auto &a, const auto &b) {
-                std::cout << "cross. a=(" << a.first << "," << a.second << "), b=(" << b.first << "," << b.second << "), result=" << (a.first * b.second - a.second * b.first) << std::endl;
                 return a.first * b.second - a.second * b.first;
             };
 
@@ -67,7 +64,6 @@ namespace isce3 { namespace geometry {
                 auto unitvec_to = deg_to_unitvec(to);
                 double angle = std::atan2(cross(unitvec_from, unitvec_to), dot(unitvec_from, unitvec_to)) * (180.0 / pi);
                 angle += angle < 0 ? 360.0 : 0.0;
-                std::cout << "angle_between. from=" << from << ", to=" << to << ", angle=" << angle << std::endl;
                 return angle;
             };
 
@@ -76,25 +72,19 @@ namespace isce3 { namespace geometry {
                 double angle_from_to = angle_between(from, to);
                 double angle_from_check = angle_between(from, check);
                 double angle_check_to = angle_between(check, to);
-                std::cout << "is_in_between. from=" << from << ", to=" << to << ", check=" << check <<  ", result=" << (std::abs(angle_from_to - (angle_from_check + angle_check_to)) < tolerance) << std::endl;
-
                 return (std::abs(angle_from_to - (angle_from_check + angle_check_to)) < tolerance);
             };
 
             // Check if this bbox contains the other bbox
-            std::cout << "Checking the current bbox" << std::endl;
             if (is_in_between(MinX, MaxX, other.MinX) && is_in_between(MinX, MaxX, other.MaxX)) {
                 // No need to do anything
-                std::cout << "Keeping the current x bounds" << std::endl;
                 return;
             }
             // Check if the other bbox contain this bbox
-            std::cout << "Checking the other bbox" << std::endl;
             if (is_in_between(other.MinX, other.MaxX, MinX) && is_in_between(other.MinX, other.MaxX, MaxX)) {
                 // Replace this bbox to others
                 MinX = other.MinX;
                 MaxX = other.MaxX;
-                std::cout << "Replacing with the other x bounds" << std::endl;
                 return;
             }
 
@@ -102,22 +92,16 @@ namespace isce3 { namespace geometry {
             double global_xmin = MinX;
             double global_xmax = MaxX;
             double span_1 = 720.0; // A number sufficiently bigger than a cycle
-            std::cout << "Checking the 1st candidate" << std::endl;
             if (is_in_between(MinX, other.MaxX, other.MinX)) {
-                std::cout << "Adding [MinX, other.MaxX] into candidate" << std::endl;
                 global_xmin = MinX;
                 global_xmax = other.MaxX;
                 span_1 = angle_between(global_xmin, global_xmax);
             }
 
             // try merging this bbox into other bbox
-            std::cout << "Checking the 2nd candidate" << std::endl;
             if (is_in_between(other.MinX, MaxX, MinX)) {
-                std::cout << "Adding [other.MinX, MaxX] into candidate" << std::endl;
                 double span_2 = angle_between(other.MinX, MaxX);
-                std::cout << "span_1=" << span_1 << ", span_2=" << span_2 << std::endl;
                 if (span_1 > span_2) {
-                    std::cout << "Choosing [other.MinX, MaxX]" << std::endl;
                     global_xmin = other.MinX;
                     global_xmax = MaxX;
                 }
