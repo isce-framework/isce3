69d363c51e77e2dc65e0e7827e8928e6501f2acd
Bo Huang <bohuang@nisar-adt-dev-3.jpl.nasa.gov>
2025-08-17 20:06:24 +0000

    Added percentage of completion print statements

--- share/nisar/examples/noise_spectra_analysis.py
+++ share/nisar/examples/noise_spectra_analysis.py
@@ -336,10 +336,8 @@ def process_l0b_data(
                 rfi_bin_hit_count = np.zeros(num_freq_bins, dtype=int)
                 rfi_bin_max_streak = np.zeros(num_freq_bins, dtype=int) 
                 
-                block_count = 0
                 # Compute averaged power spectra density and RFI hit count
                 for az_idx, az_slice in enumerate(slices):
-                    block_count += 1
                     az_blk = raw_data[az_slice, :]  # Shape: (num_pulses_blk, range_samples)
                     _, psd_az_blk = welch(
                         az_blk, 
@@ -365,8 +363,8 @@ def process_l0b_data(
                     # Write PSD of each block
                     dset_psd[az_idx, :] = avg_psd_az_blk_db
 
-                    if block_count % 10 == 0:
-                        print(f"Processed {block_count} blocks")
+                    if (az_idx + 1) % 10 == 0 or (az_idx + 1) == num_az_blocks:
+                        print(f"Processed {az_idx + 1}/{num_az_blocks} blocks ({(az_idx + 1)/num_az_blocks*100:.1f}%)")
 
                 # Write RFI Hit Count
                 if "interferenceHitCount" in out_grp:
