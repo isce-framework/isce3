61c0c970ba142c1916c470ab6144503fb33481e8
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2025-08-19 00:59:39 +0000

    Add more test case

--- tests/cxx/isce3/geometry/bbox/merge.cpp
+++ tests/cxx/isce3/geometry/bbox/merge.cpp
@@ -19,7 +19,7 @@ BoundingBox get_bbox(double minX, double maxX, double minY, double maxY) {
 }
 
 
-TEST(BboxTest, original_merge_method) {
+TEST(BboxTest, ordinary_merge_1) {
     // Test the merge method implemented in the base class of BoundingBox
     BoundingBox bbox_4326_1 = get_bbox(-10.0, 20.0, -5.0, 5.0);
     BoundingBox bbox_4326_2 = get_bbox(15.0, 30.0, 0.0, 10.0);
@@ -31,7 +31,7 @@ TEST(BboxTest, original_merge_method) {
 }
 
 
-TEST(BboxTest, ordinary_merge) {
+TEST(BboxTest, ordinary_merg_2) {
     // Simple merge in geographic coordinates that does not cross the antimeridian
     BoundingBox bbox_4326_1 = get_bbox(-10.0, 20.0, -5.0, 5.0);
     BoundingBox bbox_4326_2 = get_bbox(15.0, 30.0, 0.0, 10.0);
@@ -40,6 +40,17 @@ TEST(BboxTest, ordinary_merge) {
 
     EXPECT_DOUBLE_EQ(bbox_4326_1.MinX, -10.0);
     EXPECT_DOUBLE_EQ(bbox_4326_1.MaxX, 30.0);
+}
+
+
+TEST(BboxTest, ordinary_merge_3) {
+    BoundingBox bbox_4326_1 = get_bbox(-3.0, 3.0, -5.0, 5.0);
+    BoundingBox bbox_4326_2 = get_bbox(-2.0, 5.0, 0.0, 10.0);
+
+    bbox_4326_1.Merge(bbox_4326_2, 4326);
+
+    EXPECT_DOUBLE_EQ(bbox_4326_1.MinX, -3.0);
+    EXPECT_DOUBLE_EQ(bbox_4326_1.MaxX, 5.0);
 }
 
 
@@ -53,6 +64,19 @@ TEST(BboxTest, antimeridian_merge) {
     EXPECT_DOUBLE_EQ(bbox_4326_1.MinX, 179.0);
     EXPECT_DOUBLE_EQ(bbox_4326_1.MaxX, 183.0);
 }
+
+
+TEST(BboxTest, antimeridian_merge_2) {
+    // Simple merge in geographic coordinates that does cross the antimeridian
+    BoundingBox bbox_4326_1 = get_bbox(-177.0, 179.0, -5.0, 5.0);
+    BoundingBox bbox_4326_2 = get_bbox(179.0, 182.0, 0.0, 10.0);
+
+    bbox_4326_1.Merge(bbox_4326_2, 4326);
+
+    EXPECT_DOUBLE_EQ(bbox_4326_1.MinX, 179.0);
+    EXPECT_DOUBLE_EQ(bbox_4326_1.MaxX, 183.0);
+}
+
 
 
 TEST(BboxTest, utm_merge) {
