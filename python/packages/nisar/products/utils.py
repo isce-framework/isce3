9cdfa5c23bf32c19aa19ee9acb7ce1d29c7c4885
Gustavo H. X. Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2025-08-29 09:24:16 -0700

    fix typo

--- python/packages/nisar/products/utils.py
+++ python/packages/nisar/products/utils.py
@@ -42,7 +42,7 @@ def get_static_layers_data_access(
         The granule ID, which will be used to replace the substring
         "{granule_id}" in `static_layers_data_access_template`.
         If the template string contains "{granule_id}", but `granule_id` is
-        `None`, empty string, or "(NOT SPECIFIED)"), the function raises an
+        `None`, empty string, or "(NOT SPECIFIED)", the function raises an
         error.
 
     Returns
