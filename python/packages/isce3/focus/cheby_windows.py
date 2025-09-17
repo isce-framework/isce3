4efc51149a674035ca0f127d367c22277072ee6e
Brian P Hawkins <1729052+bhawkins@users.noreply.github.com>
2025-09-17 18:33:35 +0000

    document public API

--- python/packages/isce3/focus/cheby_windows.py
+++ python/packages/isce3/focus/cheby_windows.py
@@ -2,6 +2,9 @@ from enum import Enum, unique
 import numpy as np
 from isce3.core import KernelF32, ChebyKernelF32
 
+
+# helper classes for fitting
+
 class CosineWindowF32(KernelF32):
     def __init__(self, pedestal_height: float = 1.0):
         width = 1.0
@@ -16,6 +19,7 @@ class CosineWindowF32(KernelF32):
             return np.float32(0)
         return np.float32(self.a0 - self.a1 * np.cos(self.freq * (t + 0.5)))
 
+
 class KaiserWindowF32(KernelF32):
     def __init__(self, beta: float = 0.0):
         width = 1.0
@@ -29,12 +33,33 @@ class KaiserWindowF32(KernelF32):
         x = self.beta * np.sqrt(1 - 4 * t * t)
         return np.float32(np.i0(x) * self.scale)
 
+
 @unique
 class WindowKind(str, Enum):
     KAISER = "kaiser"
     COSINE = "cosine"
 
-def get_window_approximation(kind: WindowKind, shape, order=8) -> ChebyKernelF32:
+
+def get_window_approximation(kind: WindowKind, shape, n=8) -> ChebyKernelF32:
+    """
+    Generate a Chebyshev polynomial that approximates an apodization window.
+
+    Parameters
+    ----------
+    kind : str or WindowKind
+        Either "kaiser" or "cosine" window
+    shape : float
+        Shape parameter of the window.  Pedestal height for raised cosine
+        window or beta for Kaiser window.
+    n : int
+        Number of coefficients in the polynomial.
+
+    Returns
+    -------
+    window : isce3.core.ChebyKernelF32
+        An approximation to the desired apodization window scaled to the
+        domain [-0.5, 0.5].
+    """
     kind = WindowKind(kind)
     if kind == "kaiser":
         win = KaiserWindowF32(shape)
@@ -42,4 +67,4 @@ def get_window_approximation(kind: WindowKind, shape, order=8) -> ChebyKernelF32
         win = CosineWindowF32(shape)
     else:
         assert False, "unhandled WindowKind"
-    return ChebyKernelF32(win, order)
+    return ChebyKernelF32(win, n)
