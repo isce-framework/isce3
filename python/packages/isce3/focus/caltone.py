5d45fa6d0328adea19c32057843844e8545d145e
Brian Hawkins <1729052+bhawkins@users.noreply.github.com>
2025-10-28 18:08:10 -0700

    Improve code docs

--- python/packages/isce3/focus/caltone.py
+++ python/packages/isce3/focus/caltone.py
@@ -2,15 +2,24 @@ import numpy as np
 from isce3.signal import cola_windows
 
 class ToneRemover:
+    """
+    Coherently estimate and remove a non-stationary tone-like signal.
+    """
     def __init__(self, frequency, n, window_size=64, dtype=np.complex64):
         """
         Parameters
         ----------
         frequency : float
+            The nominal tone frequency, normalized to radians/sample, e.g.,
             (f_caltone - fc) / fs
+            Note there is no check for aliasing.
         n : int
+            Number of samples in the signal to analyze.
         window_size : int, optional
-        dtype : numpy.dtype
+            Number of samples in each estimation block.  Estimates will be
+            spaced by half the window_size.
+        dtype : numpy.dtype, optional
+            Data type for filter banks.
         """
         self.dtype = dtype
         self.size = n
@@ -23,12 +32,19 @@ class ToneRemover:
             self.wavelets.append((block, analysis, synthesis))
 
     def analyze(self, z):
+        """Estimate the time-varying response of a signal `z` to a
+        tone-like filter bank.  Akin to a single-bin STFT.
+        `z` should be one-dimensional with length self.size.
+        """
         if len(z) != self.size:
             raise ValueError(
                 f"Planned for length={self.size} but got {len(z)}")
         return np.array([w.dot(z[block]) for (block, w, _) in self.wavelets])
 
     def synthesize(self, coeffs):
+        """Generate a continuous time-domain signal given its filter bank
+        response coefficients.  Akin to a single-bin inverse STFT.
+        """
         if len(coeffs) != len(self.wavelets):
             raise ValueError("Need one coefficient per wavelet.")
         z = np.zeros(self.size, self.dtype)
@@ -37,6 +53,9 @@ class ToneRemover:
         return z
 
     def remove_tone(self, z):
+        """Estimate parameters of a time-varying tone and return the signal
+        `z` with the tone subtracted.
+        """
         if z.ndim != 1:
             raise NotImplementedError("Only 1D estimation is implemented")
         coeffs = self.analyze(z)
