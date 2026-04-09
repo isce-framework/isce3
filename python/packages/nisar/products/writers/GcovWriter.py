bd5b654cae3fbcc26c10466c626abeaab2d685e8
Gustavo H. X. Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2026-04-09 14:18:58 -0700

    add qFSP-H1 sample slip indicator to GCOV and GSLC products

--- python/packages/nisar/products/writers/GcovWriter.py
+++ python/packages/nisar/products/writers/GcovWriter.py
@@ -522,7 +522,7 @@ class GcovWriter(BaseL2WriterSingleInput):
 
             self.geocode_lut(f'{output_grids_freq_path}',
                              f'{input_swaths_freq_path}',
-                             output_ds_name_list='inputDataExceptionMask',
+                             output_ds_name_list=['inputDataExceptionMask'],
                              skip_if_not_present=True,
                              compute_stats=False,
                              data_interpolator='nearest')
