58d6c7258440866ac73df8efac4ae34ed4e0741b
Brian Hawkins <1729052+bhawkins@users.noreply.github.com>
2026-04-20 17:44:15 -0500

    Add qFSP boundary mask to RSLC (#246)
    * Add hasInputDataException to product reader
    
    * Propagate hasInputDataException from L0B to RSLC.
    
    * Add function to determine EL intervals affected by qFSP slip.
    
    * Add function for computing low-res reskew LUTs
    
    * Add method for creating mask dataset.
    
    * Add junk to nisar.cal namespace
    
    * Calculate qFSP slip mask and resample to full-size zero-Doppler grid.
    
    * Use write_direct method if available
    
    * fix loop bug
    
    * Add LUT2d::eval vectorized over both x and y
    
    * Use vectorized LUT2d.eval for reskew
    
    * Use backward-compatible type annotation
    
    * Apply suggestions from code review
    
    Co-authored-by: Brian Hawkins <1729052+bhawkins@users.noreply.github.com>
    Co-authored-by: Hirad Ghaemi <92554775+rad-eng-59@users.noreply.github.com>
    
    * Fix f-string compatibility issue.
    
    * Fix typo and some formatting
    
    ---------
    
    Co-authored-by: Hirad Ghaemi <92554775+rad-eng-59@users.noreply.github.com>

--- python/packages/nisar/workflows/focus.py
+++ python/packages/nisar/workflows/focus.py
@@ -1417,7 +1417,8 @@ def get_identification_data_from_runconfig(cfg: Struct) -> dict:
 def get_identification_data_from_raw(rawlist: list[Raw]) -> dict:
     """
     Populate a dict containing the keys
-        {"planned_datatake_id", "planned_observation_id", "is_urgent"}
+        {"planned_datatake_id", "planned_observation_id", "is_urgent",
+         "has_input_data_exception"}
     by combining the relevant identification metadata keys from all raw data
     files in the provided list.
     """
@@ -1430,7 +1431,9 @@ def get_identification_data_from_raw(rawlist: list[Raw]) -> dict:
         is_urgent = any(raw.identification.isUrgentObservation
             for raw in rawlist),
         is_joint = any(raw.identification.isJointObservation
-            for raw in rawlist)
+            for raw in rawlist),
+        has_input_data_exception = reduce(lambda a, b: a | b,
+            (raw.identification.hasInputDataException for raw in rawlist)),
     )
 
 
@@ -1727,6 +1730,25 @@ def focus(runconfig, runconfig_path=""):
                                        dem, get_rdr2geo_params(cfg))
     beta0_lut, sigma0_lut, gamma0_lut = make_cal_luts(inc_lut)
 
+    # Compute luts for zero-doppler (t,r) -> native-doppler (t,r)
+    reskew_grid = ref_grid.multilook(
+            cfg.processing.lookup_tables.downsampling_factor.azimuth,
+            cfg.processing.lookup_tables.downsampling_factor.range
+        ).add_margin(
+            cfg.processing.lookup_tables.margin_in_pixels.azimuth,
+            cfg.processing.lookup_tables.margin_in_pixels.range
+        )
+    log.info(f"Computing reskew look-up-tables with shape={reskew_grid.shape}")
+    tn_lut, rn_lut = isce3.geometry.make_reskew_lut(
+        reskew_grid.sensing_times, reskew_grid.slant_ranges, orbit, side,
+        zerodop, wvl_ref, dem=dem, doppler_out=dop_ref,
+        rdr2geo_params=get_rdr2geo_params(cfg),
+        geo2rdr_params=get_geo2rdr_params(cfg))
+
+    anomaly_code = reduce(lambda a, b: a | b,
+        (raw.identification.hasInputDataException for raw in rawlist))
+    log.info(f"Data anomaly code = {anomaly_code}")
+
     # Frequency A/B specific setup for output grid, doppler, and blocks.
     ogrid, dop, blocks_bounds, areas = dict(), dict(), dict(), dict()
     for frequency, band in get_bands(common_mode).items():
@@ -1849,6 +1871,19 @@ def focus(runconfig, runconfig_path=""):
                                         orbit.reference_epoch,
                                         extended_radar_grid.slant_ranges,
                                         beta0_lut, sigma0_lut, gamma0_lut)
+
+        # Set anomaly mask. Need to do some geometry.
+        opts = get_dataset_creation_options(cfg, og.shape)
+        del opts["dtype"]
+        mask = slc.create_anomaly_mask(frequency, shape=og.shape, **opts)
+        if instparser is not None:
+            log.info(f"Writing inputDataExceptionMask for frequency{frequency}")
+            nisar.cal.qfsp_slip.write_anomaly_mask(anomaly_code, mask,
+                og.sensing_times, og.slant_ranges, tn_lut, rn_lut, el_lut,
+                instparser)
+        else:
+            log.warning("Internal calibration (INT_CAL) file was not provided "
+                "so unable to populate inputDataExceptionMask")
 
     freq = next(iter(get_bands(common_mode)))
     slc.set_geolocation_grid(orbit, ogrid[freq], dop[freq],
