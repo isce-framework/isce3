521f933f40611b2a1cc47f14361524f56a88c34b
Gustavo H. X. Shiroma <52007211+gshiroma@users.noreply.github.com>
2026-09-17 12:16:20 -0700

    Update python/packages/nisar/products/writers/BaseL2WriterSingleInput.py
    Co-authored-by: Seongsu Jeong <27862199+seongsujeong@users.noreply.github.com>

--- python/packages/nisar/products/writers/BaseL2WriterSingleInput.py
+++ python/packages/nisar/products/writers/BaseL2WriterSingleInput.py
@@ -2277,9 +2277,7 @@ class BaseL2WriterSingleInput(BaseWriterSingleInput):
             geo = isce3.geocode.GeocodeCFloat32()
         elif input_raster_obj.datatype() == gdal.GDT_CFloat64:
             geo = isce3.geocode.GeocodeCFloat64()
-        elif (input_raster_obj.datatype() == gdal.GDT_Byte or
-              input_raster_obj.datatype() == gdal.GDT_Int16 or
-              input_raster_obj.datatype() == gdal.GDT_UInt16):
+        elif input_raster_obj.datatype() in (gdal.GDT_Byte, gdal.GDT_Int16, gdal.GDT_UInt16):
             geo = isce3.geocode.GeocodeFloat32()
         else:
             err_str = 'Unsupported raster type for geocoding'
