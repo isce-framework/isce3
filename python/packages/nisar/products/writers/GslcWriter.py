c2e7f6e094a337d7cac6312d525de20cf8ada2fc
Gustavo H. X. Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2026-09-17 17:07:10 -0700

    Set fill value of GCOV and GSLC `inputDataExceptionMask` to `65535`

--- python/packages/nisar/products/writers/GslcWriter.py
+++ python/packages/nisar/products/writers/GslcWriter.py
@@ -82,6 +82,7 @@ class GslcWriter(BaseL2WriterSingleInput):
             self.geocode_lut(f'{output_grids_freq_path}',
                              f'{input_swaths_freq_path}',
                              output_ds_name_list=['inputDataExceptionMask'],
+                             frequency=frequency,
                              skip_if_not_present=True,
                              compute_stats=False,
                              data_interpolator='nearest',
